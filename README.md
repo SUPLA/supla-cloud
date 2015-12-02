7fbf17561f27b6b6c6f5a895623be989b9fae535
Przemyslaw Zygmunt <p.zygmunt@acsoftware.pl>
2015-12-02 20:28:06 +0100

    clean

--- README.md
+++
@@ -1,4 +0,0 @@
-cloud.supla.org
-===============
-
-A Symfony project created on May 15, 2015, 9:34 am.
--- vendor/symfony/symfony/src/Symfony/Bundle/SecurityBundle/Tests/Functional/app/Acl/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-    new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-    new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-    new Symfony\Bundle\SecurityBundle\Tests\Functional\Bundle\AclBundle\AclBundle(),
-);
