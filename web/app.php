78d36a76009f3552ea6aab382c0a8cb8dacd6691
Wojciech Frącz <fraczwojciech@gmail.com>
2017-01-29 12:50:18 +0100

    Final fixes about fast self-review

--- web/app.php
+++ web/app.php
@@ -3,7 +3,8 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?: 'prod'));
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
@@ -15,11 +16,13 @@ $loader->unregister();
 $apcLoader->register(true);
 */
 
-require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__ . '/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('dev', true);
-//$kernel->loadClassCache();
+$kernel = new AppKernel(APPLICATION_ENV, APPLICATION_ENV == 'dev');
+if (APPLICATION_ENV == 'prod') {
+    $kernel->loadClassCache();
+}
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
