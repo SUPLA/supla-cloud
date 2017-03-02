15042ccab4e2dc10be6529875da4d076def309c0
Wojciech Frącz <fraczwojciech@gmail.com>
2017-03-03 00:33:45 +0100

    Fix calculating dates in mixed timezones, add tests for DST changes

--- src/SuplaBundle/Tests/Model/SchedulePlanner/CompositeSchedulePlannerTest.php
+++ src/SuplaBundle/Tests/Model/SchedulePlanner/CompositeSchedulePlannerTest.php
@@ -1,60 +1,70 @@
 <?php
 namespace SuplaBundle\Tests\Model\SchedulePlanner;
 
-use SuplaBundle\Entity\Schedule;
 use SuplaBundle\Model\SchedulePlanners\CompositeSchedulePlanner;
 use SuplaBundle\Model\SchedulePlanners\CronExpressionSchedulePlanner;
 
-class CompositeSchedulePlannerTest extends \PHPUnit_Framework_TestCase
-{
+class CompositeSchedulePlannerTest extends \PHPUnit_Framework_TestCase {
     /** @var CompositeSchedulePlanner */
     private $planner;
 
-    public function setUp()
-    {
+    public function setUp() {
         $this->planner = new CompositeSchedulePlanner([new CronExpressionSchedulePlanner()]);
     }
 
-    public function testCalculatingRunDatesUntil()
-    {
-        $schedule = new ScheduleWithTimezone();
-        $schedule->setTimeExpression('*/5 * * * *');
+    public function testCalculatingRunDatesUntil() {
+        $schedule = new ScheduleWithTimezone('*/5 * * * *', 'Europe/Warsaw');
         $runDates = array_map(function (\DateTime $d) {
-            return $d->getTimestamp();
+            return $d->format(\DateTime::ATOM);
         }, $this->planner->calculateNextRunDatesUntil($schedule, '2017-01-02 00:00', '2017-01-01 00:00'));
-        $this->assertNotContains(strtotime('2017-01-01 00:00'), $runDates);
-        $this->assertContains(strtotime('2017-01-01 00:05'), $runDates);
-        $this->assertContains(strtotime('2017-01-01 00:15'), $runDates);
-        $this->assertContains(strtotime('2017-01-01 22:35'), $runDates);
-        $this->assertContains(strtotime('2017-01-01 23:55'), $runDates);
-        $this->assertContains(strtotime('2017-01-02 00:00'), $runDates);
-        $this->assertNotContains(strtotime('2017-01-02 00:10'), $runDates);
+        $this->assertNotContains('2017-01-01T00:00:00+01:00', $runDates);
+        $this->assertContains('2017-01-01T00:05:00+01:00', $runDates);
+        $this->assertContains('2017-01-01T00:15:00+01:00', $runDates);
+        $this->assertContains('2017-01-01T22:35:00+01:00', $runDates);
+        $this->assertContains('2017-01-02T00:00:00+01:00', $runDates);
+        $this->assertNotContains('2017-01-02T00:10:00+01:00', $runDates);
     }
 
-    public function testCalculatingRunDatesUntilIfTheFirstOneIsLater()
-    {
+    public function testCalculatingWhenDstChanges() {
+        $schedule = new ScheduleWithTimezone('*/5 * * * *', 'Europe/Warsaw');
+        $runDates = array_map(function (\DateTime $d) {
+            return $d->format(\DateTime::ATOM);
+        }, $this->planner->calculateNextRunDatesUntil($schedule, '2017-03-26 04:00', '2017-03-26 01:00'));
+        $this->assertNotContains('2017-03-26T02:00:00+01:00', $runDates);
+    }
+
+    public function testCalculatingRunForMelbourne() {
+        $schedule = new ScheduleWithTimezone('*/5 * * * *', 'Australia/Melbourne');
+        $runDates = array_map(function (\DateTime $d) {
+            return $d->format(\DateTime::ATOM);
+        }, $this->planner->calculateNextRunDatesUntil($schedule, '2017-01-01 01:00', '2017-01-01 00:00'));
+        $this->assertNotContains('2017-01-01T00:00:00+11:00', $runDates);
+        $this->assertContains('2017-01-01T00:30:00+11:00', $runDates);
+        $this->assertContains('2017-01-01T01:00:00+11:00', $runDates);
+        $this->assertNotContains('2017-01-01T01:10:00+01:00', $runDates);
+    }
+
+    public function testCalculatingRunDatesUntilIfTheFirstOneIsLater() {
         $schedule = new ScheduleWithTimezone();
         $schedule->setTimeExpression('23 11 5 12 * 2089');
         $runDates = array_map(function (\DateTime $d) {
-            return $d->getTimestamp();
+            return $d->format(\DateTime::ATOM);
         }, $this->planner->calculateNextRunDatesUntil($schedule, '2017-01-01 00:00'));
         $this->assertCount(1, $runDates);
-        $this->assertContains(strtotime('2089-12-05 11:23'), $runDates);
+        $this->assertContains('2089-12-05T11:23:00+01:00', $runDates);
     }
 
-    public function testCalculatingRunDatesUntilDoesNotThrowAnErrorIfNoMoreDates()
-    {
+    public function testCalculatingRunDatesUntilDoesNotThrowAnErrorIfNoMoreDates() {
         $schedule = new ScheduleWithTimezone();
         $schedule->setTimeExpression('23 11 5 12 * 2089');
         $runDates = array_map(function ($d) {
-            return $d->getTimestamp();
+            return $d->format(\DateTime::ATOM);
         }, $this->planner->calculateNextRunDatesUntil($schedule, '2099-01-01 00:00'));
         $this->assertCount(1, $runDates);
-        $this->assertContains(strtotime('2089-12-05 11:23'), $runDates);
+        $this->assertContains('2089-12-05T11:23:00+01:00', $runDates);
     }
 
-    public function testCalculatingManyDates()
-    {
+    public function testCalculatingManyDates() {
         $schedule = new ScheduleWithTimezone();
         $schedule->setTimeExpression('*/5 * * * *');
         $dates = $this->planner->calculateNextRunDatesUntil($schedule, '+5 days');
