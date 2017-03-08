ed834344af8aef30519540c289b1b74129061c41
Wojciech Frącz <fraczwojciech@gmail.com>
2017-03-08 18:56:49 +0100

    Add schedule caption
    fix #23

--- src/SuplaBundle/Tests/Entity/ScheduleTest.php
+++ src/SuplaBundle/Tests/Entity/ScheduleTest.php
@@ -1,14 +1,25 @@
 <?php
 namespace SuplaBundle\Tests\Entity;
 
+use Assert\InvalidArgumentException;
 use SuplaBundle\Entity\Schedule;
 
-class ScheduleTest extends \PHPUnit_Framework_TestCase
-{
-    public function testSettingTheCronExpression()
-    {
+class ScheduleTest extends \PHPUnit_Framework_TestCase {
+    public function testSettingTheCronExpression() {
         $schedule = new Schedule();
         $schedule->setTimeExpression('* * * * * *');
         $this->assertEquals('* * * * * *', $schedule->getTimeExpression());
+    }
+
+    public function testFillRequiredTimeExpression() {
+        $this->setExpectedException(InvalidArgumentException::class);
+        $schedule = new Schedule();
+        $schedule->fill([]);
+    }
+
+    public function testFillFillsCaption() {
+        $schedule = new Schedule();
+        $schedule->fill(['timeExpression' => '*', 'caption' => 'My Caption']);
+        $this->assertEquals('My Caption', $schedule->getCaption());
     }
 }
