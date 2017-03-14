e779996d1a973061b34a25a40145c35af01ff966
Wojciech Frącz <fraczwojciech@gmail.com>
2017-03-14 23:01:00 +0100

    Add unit tests for setting action parameters as JSON

--- src/SuplaBundle/Tests/Entity/ScheduleTest.php
+++ src/SuplaBundle/Tests/Entity/ScheduleTest.php
@@ -3,6 +3,7 @@ namespace SuplaBundle\Tests\Entity;
 
 use Assert\InvalidArgumentException;
 use SuplaBundle\Entity\Schedule;
+use SuplaBundle\Supla\SuplaConst;
 
 class ScheduleTest extends \PHPUnit_Framework_TestCase {
     public function testSettingTheCronExpression() {
@@ -12,7 +13,7 @@ class ScheduleTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testFillRequiredTimeExpression() {
-        $this->setExpectedException(InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $schedule = new Schedule();
         $schedule->fill([]);
     }
@@ -21,5 +22,29 @@ class ScheduleTest extends \PHPUnit_Framework_TestCase {
         $schedule = new Schedule();
         $schedule->fill(['timeExpression' => '*', 'caption' => 'My Caption']);
         $this->assertEquals('My Caption', $schedule->getCaption());
+    }
+
+    public function testRequiresActionParamsForRgbLighting() {
+        $this->expectException(InvalidArgumentException::class);
+        $schedule = new Schedule();
+        $schedule->fill(['timeExpression' => '*', 'action' => SuplaConst::ACTION_SET_DIMRGBW_PARAMETERS]);
+    }
+
+    public function testSettingActionParamsAsString() {
+        $schedule = new Schedule();
+        $schedule->fill(['timeExpression' => '*', 'actionParam' => '{"color":123}']);
+        $this->assertEquals('{"color":123}', $schedule->getActionParam());
+    }
+
+    public function testSettingActionParamsAsArray() {
+        $schedule = new Schedule();
+        $schedule->fill(['timeExpression' => '*', 'actionParam' => ['color' => 123]]);
+        $this->assertEquals('{"color":123}', $schedule->getActionParam());
+    }
+
+    public function testSettingInvalidActionParam() {
+        $this->expectException(InvalidArgumentException::class);
+        $schedule = new Schedule();
+        $schedule->fill(['timeExpression' => '*', 'actionParam' => '{"color": 123']);
     }
 }
