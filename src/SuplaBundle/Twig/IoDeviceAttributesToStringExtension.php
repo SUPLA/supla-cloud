a14ec6bf9a723372e04454f4898a2ab91622d86f
Wojciech Frącz <fraczwojciech@gmail.com>
2017-03-15 14:31:49 +0100

    Translate new schedule execution statuses for user

--- src/SuplaBundle/Twig/IoDeviceAttributesToStringExtension.php
+++ src/SuplaBundle/Twig/IoDeviceAttributesToStringExtension.php
@@ -5,35 +5,30 @@ use SuplaBundle\Model\IODeviceManager;
 use SuplaBundle\Supla\SuplaConst;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class IoDeviceAttributesToStringExtension extends \Twig_Extension
-{
+class IoDeviceAttributesToStringExtension extends \Twig_Extension {
     /**
      * @var ContainerInterface
      */
     private $container;
 
-    public function __construct(ContainerInterface $container)
-    {
+    public function __construct(ContainerInterface $container) {
         $this->container = $container;
     }
 
-    public function getFilters()
-    {
+    public function getFilters() {
         return [
-            new \Twig_SimpleFilter('channelFunctionToString', array($this, 'channelFunctionToString')),
-            new \Twig_SimpleFilter('functionActionToString', array($this, 'functionActionToString')),
-            new \Twig_SimpleFilter('actionExecutionResultToString', array($this, 'actionExecutionResultToString')),
+            new \Twig_SimpleFilter('channelFunctionToString', [$this, 'channelFunctionToString']),
+            new \Twig_SimpleFilter('functionActionToString', [$this, 'functionActionToString']),
+            new \Twig_SimpleFilter('actionExecutionResultToString', [$this, 'actionExecutionResultToString']),
         ];
     }
 
-    public function channelFunctionToString($function)
-    {
+    public function channelFunctionToString($function) {
         $ioDeviceManager = $this->container->get('iodevice_manager');
         return $ioDeviceManager->channelFunctionToString($function);
     }
 
-    public function functionActionToString($action)
-    {
+    public function functionActionToString($action) {
         /** @var IODeviceManager $ioDeviceManager */
         $ioDeviceManager = $this->container->get('iodevice_manager');
         $translator = $this->container->get('translator');
@@ -41,17 +36,20 @@ class IoDeviceAttributesToStringExtension extends \Twig_Extension
         return $translator->trans($name);
     }
 
-    public function actionExecutionResultToString($result)
-    {
+    public function actionExecutionResultToString($result) {
         $text = 'To be executed';
         if ($result === SuplaConst::ACTION_EXECUTION_RESULT_SUCCESS) $text = 'Successful';
         if ($result === SuplaConst::ACTION_EXECUTION_RESULT_DEVICE_UNREACHABLE) $text = 'Device was unreachable';
         if ($result === SuplaConst::ACTION_EXECUTION_RESULT_NO_SENSOR) $text = 'Disconnected sensor';
+        if ($result === SuplaConst::ACTION_EXECUTION_RESULT_OVERDUE) $text = 'Failed';
+        if ($result === SuplaConst::ACTION_EXECUTION_RESULT_ZOMBIE) $text = 'Failed';
+        if ($result === SuplaConst::ACTION_EXECUTION_RESULT_SERVER_UNREACHABLE) $text = 'Server unreachable';
+        if ($result === SuplaConst::ACTION_EXECUTION_RESULT_FAILURE) $text = 'Failed';
+        if ($result === SuplaConst::ACTION_EXECUTION_RESULT_CANCELLED) $text = 'Cancelled';
         return $this->container->get('translator')->trans($text);
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'io_device_attributes_to_string';
     }
 }
