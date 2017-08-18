e7320a1b579ca934632a0d7322912179d9520663
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-18 12:51:15 +0200

    Add ChannelFunction from #88

---
+++ src/SuplaBundle/Enums/ChannelFunction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SuplaBundle\Enums;
+
+use MyCLabs\Enum\Enum;
+
+final class ChannelFunction extends Enum {
+    const NONE = 0;
+    const CONTROLLINGTHEGATEWAYLOCK = 10;
+    const CONTROLLINGTHEGATE = 20;
+    const CONTROLLINGTHEGARAGEDOOR = 30;
+    const THERMOMETER = 40;
+    const HUMIDITY = 42;
+    const HUMIDITYANDTEMPERATURE = 45;
+    const OPENINGSENSOR_GATEWAY = 50;
+    const OPENINGSENSOR_GATE = 60;
+    const OPENINGSENSOR_GARAGEDOOR = 70;
+    const NOLIQUIDSENSOR = 80;
+    const CONTROLLINGTHEDOORLOCK = 90;
+    const OPENINGSENSOR_DOOR = 100;
+    const CONTROLLINGTHEROLLERSHUTTER = 110;
+    const OPENINGSENSOR_ROLLERSHUTTER = 120;
+    const POWERSWITCH = 130;
+    const LIGHTSWITCH = 140;
+    const DIMMER = 180;
+    const RGBLIGHTING = 190;
+    const DIMMERANDRGBLIGHTING = 200;
+    const DEPTHSENSOR = 210;
+    const DISTANCESENSOR = 220;
+}
