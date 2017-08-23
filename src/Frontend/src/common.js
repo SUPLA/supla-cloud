459bc49b3e2772f8a430cf688fa059bd30eb2d25
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-23 10:38:42 +0200

    Synchronize browser time with server's time
    #90

--- src/Frontend/src/common.js
+++ src/Frontend/src/common.js
@@ -18,3 +18,13 @@ Vue.http.options.root = Vue.config.external.baseUrl || '';
 
 moment.locale(Vue.config.external.locale);
 moment.tz.setDefault(Vue.config.external.timezone);
+
+// synchronize browser time with server's
+(function () {
+    const serverTime = new Date(window.FRONTEND_CONFIG.serverTime);
+    const renderStart = window.FRONTEND_CONFIG.renderStart;
+    const offset = serverTime.getTime() - renderStart.getTime();
+    moment.now = function () {
+        return Date.now() + offset;
+    };
+})();
