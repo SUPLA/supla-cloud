5951c1d003631590702fa814940bf150372b11e3
Wojciech Frącz <fraczwojciech@gmail.com>
2017-08-25 15:42:37 +0200

    Add global HTTP request interceptor that display an error notification on invalid response
    #90

--- src/Frontend/src/common.js
+++ src/Frontend/src/common.js
@@ -4,6 +4,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import VueI18N from "vue-i18n";
 import VueResource from "vue-resource";
+import RespnseErrorInterceptor from "./common/response-error-interceptor";
 import "moment-timezone";
 import "./common/common-components";
 import "./common/filters";
@@ -15,6 +16,8 @@ Vue.use(VueResource);
 
 Vue.config.external = window.FRONTEND_CONFIG || {};
 Vue.http.options.root = Vue.config.external.baseUrl || '';
+
+Vue.http.interceptors.push(RespnseErrorInterceptor);
 
 moment.locale(Vue.config.external.locale);
 moment.tz.setDefault(Vue.config.external.timezone);
--- src/Frontend/src/common/common-components.js
+++ src/Frontend/src/common/common-components.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Modal from "./modal.vue";
 import ModalConfirm from "./modal-confirm.vue";
-import "./notifier";
 
 Vue.component('modal', Modal);
 Vue.component('modalConfirm', ModalConfirm);
