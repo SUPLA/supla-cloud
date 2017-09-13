c604593283e3c328f330e8db722df9057a333c00
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-13 21:11:00 +0200

    Safe-insert GET parameter to URL when changing language

--- src/Frontend/src/login/language-selector.vue
+++ src/Frontend/src/login/language-selector.vue
@@ -28,7 +28,27 @@
         },
         methods: {
             updateLocale() {
-                window.location.assign(window.location.href + '?lang=' + this.locale);
+                this.safeInsertGetParamToUrl('lang', this.locale);
+            },
+            // idea based on https://stackoverflow.com/a/487049/878514
+            safeInsertGetParamToUrl(key, value) {
+                key = encodeURI(key);
+                value = encodeURI(value);
+                const kvp = document.location.search.substr(1).split('&');
+                let i = kvp.length;
+                let x;
+                while (i--) {
+                    x = kvp[i].split('=');
+                    if (x[0] == key) {
+                        x[1] = value;
+                        kvp[i] = x.join('=');
+                        break;
+                    }
+                }
+                if (i < 0) {
+                    kvp[kvp.length] = [key, value].join('=');
+                }
+                document.location.search = kvp.join('&');
             }
         }
     };
