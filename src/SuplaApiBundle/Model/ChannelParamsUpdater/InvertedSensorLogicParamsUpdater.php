f13fce5a97157a932c5f956d1f8bbfc5110f77d1
Wojciech Frącz <fraczwojciech@gmail.com>
2018-02-24 14:00:00 +0100

    Reformat multiline constructor call

--- src/SuplaApiBundle/Model/ChannelParamsUpdater/InvertedSensorLogicParamsUpdater.php
+++ src/SuplaApiBundle/Model/ChannelParamsUpdater/InvertedSensorLogicParamsUpdater.php
@@ -5,19 +5,15 @@ use SuplaBundle\Enums\ChannelFunction;
 
 class InvertedSensorLogicParamsUpdater extends RangeParamsUpdater {
     public function __construct() {
-        parent::__construct(
-            [
-                ChannelFunction::OPENINGSENSOR_WINDOW(),
-                ChannelFunction::OPENINGSENSOR_GATEWAY(),
-                ChannelFunction::OPENINGSENSOR_GATE(),
-                ChannelFunction::OPENINGSENSOR_GARAGEDOOR(),
-                ChannelFunction::OPENINGSENSOR_DOOR(),
-                ChannelFunction::OPENINGSENSOR_ROLLERSHUTTER(),
-                ChannelFunction::NOLIQUIDSENSOR(),
-                ChannelFunction::MAILSENSOR(),
-            ],
-            0,
-            1,
-            3);
+        parent::__construct([
+            ChannelFunction::OPENINGSENSOR_WINDOW(),
+            ChannelFunction::OPENINGSENSOR_GATEWAY(),
+            ChannelFunction::OPENINGSENSOR_GATE(),
+            ChannelFunction::OPENINGSENSOR_GARAGEDOOR(),
+            ChannelFunction::OPENINGSENSOR_DOOR(),
+            ChannelFunction::OPENINGSENSOR_ROLLERSHUTTER(),
+            ChannelFunction::NOLIQUIDSENSOR(),
+            ChannelFunction::MAILSENSOR(),
+        ], 0, 1, 3);
     }
 }
