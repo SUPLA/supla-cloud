0356a3d0ce661e2df0d5f8c65ae46f5d89e41952
Wojciech Frącz <fraczwojciech@gmail.com>
2018-03-10 11:21:43 +0100

    Smooth apply changes buttons transition

--- src/Frontend/src/common/pages/pending-changes-page.vue
+++ src/Frontend/src/common/pages/pending-changes-page.vue
@@ -2,27 +2,32 @@
     <form @submit.prevent="$emit('save')">
         <div class="clearfix left-right-header">
             <h2 class="no-margin-top">{{ header }}</h2>
-            <div class="btn-toolbar no-margin-top"
-                v-if="isPending">
-                <a class="btn btn-grey"
-                    @click="$emit('cancel')">
-                    <i class="pe-7s-back"></i>
-                    {{ $t('Cancel changes') }}
-                </a>
-                <button class="btn btn-yellow btn-lg"
-                    type="submit">
-                    <i class="pe-7s-diskette"></i>
-                    {{ $t('Save changes') }}
-                </button>
+            <div class="button-container no-margin-top">
+                <transition name="fade">
+                    <div class="btn-toolbar"
+                        v-if="isPending">
+                        <button class="btn btn-white"
+                            type="submit">
+                            <i class="pe-7s-diskette"></i>
+                            {{ $t('Save changes') }}
+                        </button>
+                        <a class="btn btn-grey"
+                            @click="$emit('cancel')">
+                            <i class="pe-7s-back"></i>
+                            {{ $t('Cancel changes') }}
+                        </a>
+                    </div>
+                </transition>
+                <transition name="fade">
+                    <div class="btn-toolbar"
+                        v-if="!isPending && deletable">
+                        <a class="btn btn-danger"
+                            @click="$emit('delete')">
+                            {{ $t('Delete') }}
+                        </a>
+                    </div>
+                </transition>
             </div>
-            <div class="btn-toolbar no-margin-top"
-                v-else-if="deletable">
-                <a class="btn btn-danger"
-                    @click="$emit('delete')">
-                    {{ $t('Delete') }}
-                </a>
-            </div>
-            <div v-else></div>
         </div>
         <div class="form-group">
             <slot></slot>
@@ -31,16 +36,16 @@
             <transition name="fade">
                 <div class="btn-toolbar"
                     v-if="isPending">
+                    <button class="btn btn-white"
+                        type="submit">
+                        <i class="pe-7s-diskette"></i>
+                        {{ $t('Save changes') }}
+                    </button>
                     <a class="btn btn-grey"
                         @click="$emit('cancel')">
                         <i class="pe-7s-back"></i>
                         {{ $t('Cancel changes') }}
                     </a>
-                    <button class="btn btn-yellow btn-lg"
-                        type="submit">
-                        <i class="pe-7s-diskette"></i>
-                        {{ $t('Save changes') }}
-                    </button>
                 </div>
             </transition>
         </div>
@@ -48,8 +53,34 @@
 </template>
 
 <script>
-
     export default {
         props: ['header', 'deletable', 'isPending'],
     };
 </script>
+
+
+<style lang="scss">
+    @import "../../styles/mixins";
+
+    .button-container {
+        position: relative;
+        .btn-toolbar {
+            position: absolute;
+            right: 0;
+            min-width: 350px;
+            .btn {
+                float: right;
+            }
+
+        }
+        @include on-xs-and-down {
+            height: 35px;
+            .btn-toolbar {
+                width: 100%;
+            }
+            .btn {
+                float: none !important;
+            }
+        }
+    }
+</style>
