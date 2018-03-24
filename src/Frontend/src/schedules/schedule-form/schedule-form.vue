4cbe1e93ff171c77b9b5657d8058a3a104b1131b
Wojciech Frącz <fraczwojciech@gmail.com>
2018-03-25 00:39:21 +0100

    Move schedule form buttons to the top

--- src/Frontend/src/schedules/schedule-form/schedule-form.vue
+++ src/Frontend/src/schedules/schedule-form/schedule-form.vue
@@ -1,88 +1,74 @@
 <template>
     <page-container :error="error"
         class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <h1 v-if="id">
-                    <span v-title>{{ $t('Edit schedule') }} ID{{ id }}</span>
-                    <span class="small"
-                        v-if="schedule.caption">{{ schedule.caption }}</span>
-                </h1>
-                <h1 v-else
-                    v-title>{{ $t('Create New Schedule') }}</h1>
-            </div>
-        </div>
-        <loading-dots v-if="id && !schedule.id"></loading-dots>
-        <div v-else>
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        <label>{{ $t("Name") }}</label>
-                        <input type="text"
-                            class="form-control"
-                            v-model="caption">
-                    </div>
-                    <div class="form-group">
-                        <label>{{ $t("Schedule mode") }}</label>
-                        <div class="clearfix"></div>
-                        <schedule-mode-chooser></schedule-mode-chooser>
-                    </div>
+        <loading-cover :loading="submitting || (id && !schedule.id)">
+            <pending-changes-page :header="id ? $t('Edit schedule') + ' ID' + id : $t('Create New Schedule')"
+                v-if="schedule">
+                <div slot="buttons">
+                    <router-link :to="{name: 'schedule', params: schedule}"
+                        class="btn btn-white"
+                        v-if="schedule.id">
+                        <i class="pe-7s-back"></i>
+                        {{ $t('Cancel') }}
+                    </router-link>
+                    <button class="btn btn-white"
+                        v-if="schedule.enabled === false"
+                        :disabled="actionId == undefined || !nextRunDates.length || fetchingNextRunDates"
+                        @click="submit()">
+                        <i class="pe-7s-diskette"></i>
+                        {{ $t('Save') }}
+                    </button>
+                    <button class="btn btn-green"
+                        :disabled="actionId == undefined || !nextRunDates.length || fetchingNextRunDates"
+                        @click="submit(true)">
+                        <i :class="id ? 'pe-7s-diskette' : 'pe-7s-plus'"></i>
+                        {{ $t(id ? (schedule.enabled ? 'Save' : 'Save and enable') : 'Add') }}
+                    </button>
                 </div>
-            </div>
-            <div class="row"
-                v-show="mode">
-                <div class="col-md-6">
-                    <div class="well">
-                        <h3 class="no-margin-top">{{ $t('When') }}?</h3>
+                <div class="row">
+                    <div class="col-sm-12">
                         <div class="form-group">
-                            <schedule-form-mode-once v-if="mode == 'once'"></schedule-form-mode-once>
-                            <schedule-form-mode-minutely v-if="mode == 'minutely'"></schedule-form-mode-minutely>
-                            <schedule-form-mode-hourly v-if="mode == 'hourly'"></schedule-form-mode-hourly>
-                            <schedule-form-mode-daily v-if="mode == 'daily'"></schedule-form-mode-daily>
+                            <label>{{ $t("Name") }}</label>
+                            <input type="text"
+                                class="form-control"
+                                v-model="caption">
                         </div>
-                        <div v-if="mode != 'once'">
-                            <schedule-form-start-end-date></schedule-form-start-end-date>
-                        </div>
-                        <next-run-dates-preview></next-run-dates-preview>
-                        <toggler v-model="retry"
-                            v-if="canSetRetry"
-                            label="Retry when fail"></toggler>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="well">
-                        <h3 class="no-margin-top">{{ $t('Action') }}</h3>
-                        <schedule-form-action-chooser @channel-change="canSetRetry = !$event || [20, 30].indexOf($event.function) < 0"></schedule-form-action-chooser>
-                        <div class="text-right"
-                            v-if="!submitting">
-                            <router-link :to="{name: 'schedule', params: schedule}"
-                                class="btn btn-white"
-                                v-if="schedule.id">
-                                <i class="pe-7s-back-2"></i>
-                                {{ $t('Cancel') }}
-                            </router-link>
-                            <button class="btn btn-white btn-lg"
-                                v-if="schedule.enabled === false"
-                                :disabled="actionId == undefined || !nextRunDates.length || fetchingNextRunDates"
-                                @click="submit()">
-                                <i class="pe-7s-diskette"></i>
-                                {{ $t('Save') }}
-                            </button>
-                            <button class="btn btn-green btn-lg"
-                                :disabled="actionId == undefined || !nextRunDates.length || fetchingNextRunDates"
-                                @click="submit(true)">
-                                <i :class="id ? 'pe-7s-diskette' : 'pe-7s-plus'"></i>
-                                {{ $t(id ? (schedule.enabled ? 'Save' : 'Save and enable') : 'Add') }}
-                            </button>
-                        </div>
-                        <div class="text-right"
-                            v-if="submitting">
-                            <button-loading></button-loading>
+                        <div class="form-group">
+                            <label>{{ $t("Schedule mode") }}</label>
+                            <div class="clearfix"></div>
+                            <schedule-mode-chooser></schedule-mode-chooser>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <div class="row"
+                    v-show="mode">
+                    <div class="col-md-6">
+                        <div class="well">
+                            <h3 class="no-margin-top">{{ $t('When') }}?</h3>
+                            <div class="form-group">
+                                <schedule-form-mode-once v-if="mode == 'once'"></schedule-form-mode-once>
+                                <schedule-form-mode-minutely v-if="mode == 'minutely'"></schedule-form-mode-minutely>
+                                <schedule-form-mode-hourly v-if="mode == 'hourly'"></schedule-form-mode-hourly>
+                                <schedule-form-mode-daily v-if="mode == 'daily'"></schedule-form-mode-daily>
+                            </div>
+                            <div v-if="mode != 'once'">
+                                <schedule-form-start-end-date></schedule-form-start-end-date>
+                            </div>
+                            <next-run-dates-preview></next-run-dates-preview>
+                            <toggler v-model="retry"
+                                v-if="canSetRetry"
+                                label="Retry when fail"></toggler>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="well">
+                            <h3 class="no-margin-top">{{ $t('Action') }}</h3>
+                            <schedule-form-action-chooser @channel-change="canSetRetry = !$event || [20, 30].indexOf($event.function) < 0"></schedule-form-action-chooser>
+                        </div>
+                    </div>
+                </div>
+            </pending-changes-page>
+        </loading-cover>
     </page-container>
 </template>
 
@@ -103,6 +89,7 @@
     import {actions, mutations} from "./schedule-form-store";
     import Toggler from "../../common/gui/toggler";
     import PageContainer from "../../common/pages/page-container";
+    import PendingChangesPage from "../../common/pages/pending-changes-page";
 
     export default {
         name: 'schedule-form',
@@ -165,6 +152,7 @@
             }
         },
         components: {
+            PendingChangesPage,
             PageContainer,
             ScheduleModeChooser,
             ScheduleFormModeOnce,
