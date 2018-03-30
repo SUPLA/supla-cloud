ba6b0028293f97f9ef4cc101c4a8c3e87612cdb2
Przemek Zygmunt <przemek@supla.org>
2018-03-30 12:19:42 +0200

    Correction of translations

--- src/Frontend/src/common/errors/error-403.vue
+++ src/Frontend/src/common/errors/error-403.vue
@@ -3,7 +3,7 @@
         <h1>{{ $t('Seems like a secret place') }}</h1>
         <i class="pe-7s-door-lock"
             style="font-size: 160px"></i>
-        <h5>{{ $t('The server has returned a HTTP 403 Error. It means that you don\'t have rights to display the item you are looking for.') }}</h5>
+        <h5>{{ $t('The server has returned an HTTP 403 Error. It means that you are not authorized to display the item you are looking for.') }}</h5>
     </div>
 </template>
 
--- src/Frontend/src/common/errors/error-404.vue
+++ src/Frontend/src/common/errors/error-404.vue
@@ -4,7 +4,7 @@
             <h1>{{ $t('Are you lost?') }}</h1>
             <i class="pe-7s-way"
                 style="font-size: 160px"></i>
-            <h5>{{ $t('The server has returned a HTTP 404 Error. It means that the item you are looking for does not exist.') }}</h5>
+            <h5>{{ $t('The server has returned an HTTP 404 Error. It means that the item you are looking for does not exist.') }}</h5>
         </div>
         <login-footer v-if="!$user"></login-footer>
     </div>
