5723be37ff69a9f3f87132c47b8c454824f0089e
Wojciech Frącz <fraczwojciech@gmail.com>
2018-04-09 10:03:09 +0200

    Separate UserProvider and UserRepository

--- src/SuplaBundle/Repository/UserRepository.php
+++ src/SuplaBundle/Repository/UserRepository.php
@@ -2,41 +2,10 @@
 namespace SuplaBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use SuplaApiBundle\Model\Audit\FailedAuthAttemptsUserBlocker;
 use SuplaBundle\Entity\User;
-use Symfony\Component\Security\Core\Exception\LockedException;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 /**
  * @method User|null findOneByEmail(string $email)
  */
-class UserRepository extends EntityRepository implements UserProviderInterface {
-    /** @var FailedAuthAttemptsUserBlocker */
-    private $failedAuthAttemptsUserBlocker;
-
-    /** @required */
-    public function setFailedAuthAttemptsUserBlocker(FailedAuthAttemptsUserBlocker $failedAuthAttemptsUserBlocker) {
-        $this->failedAuthAttemptsUserBlocker = $failedAuthAttemptsUserBlocker;
-    }
-
-    public function loadUserByUsername($username) {
-        $user = $this->findOneByEmail($username);
-        if ($this->failedAuthAttemptsUserBlocker->isAuthenticationFailureLimitExceeded($username)) {
-            throw new LockedException();
-        }
-        if (!$user) {
-            throw new UsernameNotFoundException();
-        }
-        return $user;
-    }
-
-    public function refreshUser(UserInterface $user) {
-        $this->getEntityManager()->refresh($user);
-    }
-
-    public function supportsClass($class) {
-        return $class === User::class;
-    }
+class UserRepository extends EntityRepository {
 }
