83c0f2bcf6632dd06b78e7103f0e38f4dd74292c
Wojciech Frącz <fraczwojciech@gmail.com>
2018-04-11 22:50:20 +0200

    Refresh the browser when the user has been logged out (session expired or sth)

--- src/Frontend/src/common/http/response-error-interceptor.js
+++ src/Frontend/src/common/http/response-error-interceptor.js
@@ -7,10 +7,14 @@ export default function (vue) {
                 const skip = request.skipErrorHandler &&
                     (!Array.isArray(request.skipErrorHandler) || request.skipErrorHandler.indexOf(response.status) >= 0);
                 if (!skip) {
-                    const message = (response.body && response.body.message)
-                        || 'Error when communicating the server. Try again in a while.';
-                    const details = (response.body && response.body.details) || {};
-                    errorNotification(vue.$t('Error'), vue.$t(message, details));
+                    if (response.status == 401) {
+                        window.location.assign(window.location.toString());
+                    } else {
+                        const message = (response.body && response.body.message)
+                            || 'Error when communicating the server. Try again in a while.';
+                        const details = (response.body && response.body.details) || {};
+                        errorNotification(vue.$t('Error'), vue.$t(message, details));
+                    }
                 }
             }
         });
