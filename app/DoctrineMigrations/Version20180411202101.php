46aa6c6842d05dac294eeddfc00dd0b2f52b4e87
Przemek Zygmunt <przemek@supla.org>
2018-04-11 20:28:49 +0200

    New views supporting channel groups

---
+++ app/DoctrineMigrations/Version20180411202101.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add views for channel group processing
+ */
+class Version20180411202101 extends AbstractMigration {
+    public 
+
+
+function up(Schema $schema) {
+	$this->abortIf ( $this->connection->getDatabasePlatform ()->getName () !== 'mysql', 'Migration can only be executed safely on \'mysql\'.' );
+	$this->addSql ( <<<CREATE_VIEW
+				CREATE
+				 ALGORITHM = UNDEFINED
+				 VIEW `supla_v_client_channel_group`
+				 AS SELECT `g`.`id` AS `id`,
+				 `g`.`func` AS `func`,
+				 `g`.`caption` AS `caption`,
+				 `g`.`user_id` AS `user_id`,
+				 `g`.`location_id`,
+				 ifnull(`g`.`alt_icon`,0) AS `alt_icon`,
+				 `cl`.`id` AS `client_id`
+				FROM `supla`.`supla_dev_channel_group` `g`
+				  JOIN `supla`.`supla_location` `l` on(`l`.`id` = `g`.`location_id`)
+				  JOIN `supla`.`supla_rel_aidloc` `r` on(`r`.`location_id` = `l`.`id`)
+				  JOIN `supla`.`supla_accessid` `a` on(`a`.`id` = `r`.`access_id`)
+				  JOIN `supla`.`supla_client` `cl` on(`cl`.`access_id` = `r`.`access_id`)
+				WHERE ((`g`.`func` IS NOT NULL)
+				  AND (ifnull(`g`.`hidden`,0) = 0)
+				  AND (`g`.`func` <> 0)
+				  AND (`l`.`enabled` = 1)
+				  AND (`a`.`enabled` = 1))
+CREATE_VIEW
+ );
+	
+	$this->addSql ( <<<CREATE_VIEW
+				CREATE
+				 ALGORITHM = UNDEFINED
+				 VIEW `supla_v_rel_cg`
+				 AS SELECT `r`.`group_id` AS `group_id`,`r`.`channel_id` AS `channel_id`,
+                 `c`.`iodevice_id` AS `iodevice_id`,`d`.`protocol_version` AS `protocol_version`,
+                 `g`.`client_id` AS `client_id`,`c`.`hidden` AS `channel_hidden` from 
+                 (((`supla`.`supla_v_client_channel_group` `g` join `supla`.`supla_rel_cg` `r` on((`r`.`group_id` = `g`.`id`))) 
+                 join `supla`.`supla_dev_channel` `c` on((`c`.`id` = `r`.`channel_id`))) 
+                 join `supla`.`supla_iodevice` `d` on((`d`.`id` = `c`.`iodevice_id`))) where `d`.`enabled` = 1
+CREATE_VIEW
+ );
+}
+
+    public function down(Schema $schema) {
+        $this->abortIf(true, 'There is no way back');
+    }
+}
