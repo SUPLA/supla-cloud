501350a5312d59cf3123fa24b4761e3d8e87ee2a
Wojciech Frącz <fraczwojciech@gmail.com>
2018-07-07 00:19:07 +0200

    Append base64-encoded supla cloud URL to OAuth2 tokens

---
+++ src/SuplaApiBundle/Auth/SuplaOAuth2.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+namespace SuplaApiBundle\Auth;
+
+use OAuth2\IOAuth2Storage;
+use OAuth2\OAuth2;
+
+class SuplaOAuth2 extends OAuth2 {
+    /** @var string */
+    private $suplaUrl;
+
+    public function __construct(IOAuth2Storage $storage, array $config = [], string $suplaUrl = '') {
+        parent::__construct($storage, $config);
+        $this->suplaUrl = $suplaUrl;
+    }
+
+    protected function genAccessToken() {
+        return parent::genAccessToken() . '.' . base64_encode($this->suplaUrl);
+    }
+}
