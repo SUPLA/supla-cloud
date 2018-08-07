6b9361e282e9efd9a01ff9b22bb4cd2c951e2a68
Przemek Zygmunt <przemek@supla.org>
2018-08-07 12:48:38 +0200

    NOT NULL -> DEFAULT NULL

--- app/DoctrineMigrations/Version20180723132652.php
+++ app/DoctrineMigrations/Version20180723132652.php
@@ -18,7 +18,7 @@ class Version20180723132652 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE supla_em_log (id INT AUTO_INCREMENT NOT NULL, channel_id INT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:utcdatetime)\', phase1_fae BIGINT NOT NULL, phase1_rae BIGINT NOT NULL, phase1_fre BIGINT NOT NULL, phase1_rre BIGINT NOT NULL, phase2_fae BIGINT NOT NULL, phase2_rae BIGINT NOT NULL, phase2_fre BIGINT NOT NULL, phase2_rre BIGINT NOT NULL, phase3_fae BIGINT NOT NULL, phase3_rae BIGINT NOT NULL, phase3_fre BIGINT NOT NULL, phase3_rre BIGINT NOT NULL, INDEX channel_id_idx (channel_id), INDEX date_idx (date), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE supla_em_log (id INT AUTO_INCREMENT NOT NULL, channel_id INT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:utcdatetime)\', phase1_fae BIGINT DEFAULT NULL, phase1_rae BIGINT DEFAULT NULL, phase1_fre BIGINT DEFAULT NULL, phase1_rre BIGINT DEFAULT NULL, phase2_fae BIGINT DEFAULT NULL, phase2_rae BIGINT DEFAULT NULL, phase2_fre BIGINT DEFAULT NULL, phase2_rre BIGINT DEFAULT NULL, phase3_fae BIGINT NOT NULL, phase3_rae BIGINT NOT NULL, phase3_fre BIGINT NOT NULL, phase3_rre BIGINT NOT NULL, INDEX channel_id_idx (channel_id), INDEX date_idx (date), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     /**
