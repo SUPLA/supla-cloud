b5e6530fd0a3f8162be1381af5c48b9ff415fe2d
Przemek Zygmunt <przemek@supla.org>
2018-09-15 16:15:29 +0200

    Unit tests for Electricity Measurement API. Closes #238

--- src/SuplaBundle/Tests/Integration/Controller/ChannelMeasurementLogsControllerIntegrationTest.php
+++ src/SuplaBundle/Tests/Integration/Controller/ChannelMeasurementLogsControllerIntegrationTest.php
@@ -20,6 +20,8 @@ namespace SuplaBundle\Tests\Integration\Controller;
 use SuplaBundle\Entity\EntityUtils;
 use SuplaBundle\Entity\IODevice;
 use SuplaBundle\Entity\TemperatureLogItem;
+use SuplaBundle\Entity\TempHumidityLogItem;
+use SuplaBundle\Entity\ElectricityMeterLogItem;
 use SuplaBundle\Entity\User;
 use SuplaBundle\Enums\ChannelFunction;
 use SuplaBundle\Enums\ChannelType;
@@ -37,32 +39,83 @@ class ChannelMeasurementLogsControllerIntegrationTest extends IntegrationTestCas
     private $device;
 
     protected function setUp() {
+        
+        $datestr = '2018-09-15';
+        $date = new \DateTime($datestr);
+        $oneday = new \DateInterval('P1D');
+       
         $this->user = $this->createConfirmedUser();
         $location = $this->createLocation($this->user);
         $this->device = $this->createDevice($location, [
             [ChannelType::RELAY, ChannelFunction::LIGHTSWITCH],
             [ChannelType::THERMOMETER, ChannelFunction::THERMOMETER],
+            [ChannelType::HUMIDITYANDTEMPSENSOR, ChannelFunction::HUMIDITYANDTEMPERATURE],
+            [ChannelType::ELECTRICITYMETER, ChannelFunction::ELECTRICITYMETER],
         ]);
+     
+        $date = new \DateTime($datestr);
         foreach ([21, 22, 23] as $temperature) {
             $logItem = new TemperatureLogItem();
             EntityUtils::setField($logItem, 'channel_id', 2);
-            EntityUtils::setField($logItem, 'date', new \DateTime());
+            EntityUtils::setField($logItem, 'date', clone $date);
             EntityUtils::setField($logItem, 'temperature', $temperature);
             $this->getEntityManager()->persist($logItem);
+            $date->add($oneday);
         }
+        
+        $date = new \DateTime($datestr);
+        foreach ([[21,30], [22, 40], [23, 50]] as $th) {
+            $logItem = new TempHumidityLogItem();
+            EntityUtils::setField($logItem, 'channel_id', 3);
+            EntityUtils::setField($logItem, 'date', clone $date);
+            EntityUtils::setField($logItem, 'temperature', $th[0]);
+            EntityUtils::setField($logItem, 'humidity', $th[1]);
+            $this->getEntityManager()->persist($logItem);
+            $date->add($oneday);
+        }
+        
+        $date = new \DateTime($datestr);
+        foreach ([854800,854900,855000] as $energy) {
+            $logItem = new ElectricityMeterLogItem();
+            EntityUtils::setField($logItem, 'channel_id', 4);
+            EntityUtils::setField($logItem, 'date', clone $date);
+            
+            foreach ([1,2,3] as $phase) {
+                EntityUtils::setField($logItem, 'phase'.$phase.'_fae', $energy+$phase);
+                EntityUtils::setField($logItem, 'phase'.$phase.'_rae', $energy+$phase+10);
+                EntityUtils::setField($logItem, 'phase'.$phase.'_fre', $energy+$phase+20);
+                EntityUtils::setField($logItem, 'phase'.$phase.'_rre', $energy+$phase+30);
+            }
+            
+            $this->getEntityManager()->persist($logItem);
+            $date->add($oneday);
+        }
+  
         $this->getEntityManager()->flush();
     }
-
-    public function testGettingMeasurementLogsCount() {
+    
+    private function testGettingMeasurementLogsCount(int $channelId) {
         $client = $this->createAuthenticatedClient($this->user);
-        $client->apiRequestV22('GET', '/api/channels/2/measurement-logs');
+        $client->apiRequestV22('GET', '/api/channels/'.$channelId.'/measurement-logs');
         $response = $client->getResponse();
         $this->assertStatusCode('2xx', $response);
         $this->assertTrue($response->headers->has('X-Total-Count'));
         $this->assertEquals(3, $response->headers->get('X-Total-Count'));
     }
 
-    public function testGettingMeasurementLogsCountObsolete() {
+    public function testGettingTemperatureLogsCount() {
+        $this->testGettingMeasurementLogsCount(2);
+    }
+    
+    public function testGettingTemperatureAndHumidityLogsCount() {
+        $this->testGettingMeasurementLogsCount(3);
+    }
+    
+    public function testGettingElectricityMeasurementsLogsCount() {
+        $this->testGettingMeasurementLogsCount(4);
+    }
+    
+    public function testGettingTemperatureLogsCountObsolete() {
         $client = $this->createAuthenticatedClient($this->user);
         $client->apiRequest('GET', '/api/channels/2/temperature-log-count');
         $response = $client->getResponse();
@@ -70,26 +123,150 @@ class ChannelMeasurementLogsControllerIntegrationTest extends IntegrationTestCas
         $content = json_decode($response->getContent(), true);
         $this->assertEquals(3, $content['count']);
     }
-
-    public function testGettingMeasurementLogs() {
+    
+    public function testGettingTemperatureAnHumidityLogsCountObsolete() {
         $client = $this->createAuthenticatedClient($this->user);
-        $client->apiRequestV22('GET', '/api/channels/2/measurement-logs');
+        $client->apiRequest('GET', '/api/channels/3/temperature-and-humidity-count');
+        $response = $client->getResponse();
+        $this->assertStatusCode('2xx', $response);
+        $content = json_decode($response->getContent(), true);
+        $this->assertEquals(3, $content['count']);
+    }
+    
+    private function testGettingMeasurementLogs(int $channelId) {
+        $client = $this->createAuthenticatedClient($this->user);
+        $client->apiRequestV22('GET', '/api/channels/'.$channelId.'/measurement-logs');
         $response = $client->getResponse();
         $this->assertStatusCode('2xx', $response);
         $content = json_decode($response->getContent(), true);
         $this->assertCount(3, $content);
+        return $content;
+    }
+    
+    private function emIncrement(array &$values, int $inc) {
+        array_walk($values, function (&$value, $key) use ($inc) {
+            $value += $inc;
+        });
+    }
+    
+    private function testElectricityMeasurementLogsOrder(array $content, array $energiesInOrder) {
+        foreach ([1,2,3] as $phase) {
+            $eInOrder = $energiesInOrder;
+            array_walk($eInOrder, function (&$value, $key) use ($phase) {
+                $value += $phase;
+            });
+            $this->emIncrement($eInOrder, 10);
+            $this->assertEquals($eInOrder, array_map('intval', array_column($content, 'phase'.$phase.'_rae')));
+            $this->emIncrement($eInOrder, 10);
+            $this->assertEquals($eInOrder, array_map('intval', array_column($content, 'phase'.$phase.'_fre')));
+            $this->emIncrement($eInOrder, 10);
+            $this->assertEquals($eInOrder, array_map('intval', array_column($content, 'phase'.$phase.'_rre')));
+        }
+    }
+
+    public function testGettingTemperatureLogs() {
+        $content = $this->testGettingMeasurementLogs(2);
         $temperaturesInOrder = [23, 22, 21];
         $this->assertEquals($temperaturesInOrder, array_map('intval', array_column($content, 'temperature')));
     }
-
-    public function testGettingMeasurementLogsWithOffset() {
+    
+    public function testGettingTemperatureAndHumidityLogs() {
+        $content = $this->testGettingMeasurementLogs(3);
+        $temperaturesInOrder = [23, 22, 21];
+        $this->assertEquals($temperaturesInOrder, array_map('intval', array_column($content, 'temperature')));
+        $humiditiesInOrder = [50, 40, 30];
+        $this->assertEquals($humiditiesInOrder, array_map('intval', array_column($content, 'humidity')));
+    }
+        
+    public function testGettingElectricityMeasurementLogs() {
+        $content = $this->testGettingMeasurementLogs(4);
+        $this->testElectricityMeasurementLogsOrder($content, [855000,854900,854800]);
+    }
+    
+    private function testGettingMeasurementLogsAscending(int $channelId) {
         $client = $this->createAuthenticatedClient($this->user);
-        $client->apiRequestV22('GET', '/api/channels/2/measurement-logs?offset=1&limit=1');
+        $client->apiRequestV22('GET', '/api/channels/'.$channelId.'/measurement-logs?order=ASC');
+        $response = $client->getResponse();
+        $this->assertStatusCode('2xx', $response);
+        $content = json_decode($response->getContent(), true);
+        $this->assertCount(3, $content);
+        return $content;
+    }
+    
+    public function testGettingTemperatureLogsAscending() {
+        $content = $this->testGettingMeasurementLogsAscending(2);
+        $temperaturesInOrder = [21, 22, 23];
+        $this->assertEquals($temperaturesInOrder, array_map('intval', array_column($content, 'temperature')));
+    }
+    
+    public function testGettingTemperatureAndHumidityLogsAscending() {
+        $content = $this->testGettingMeasurementLogsAscending(3);
+        $temperaturesInOrder = [21, 22, 23];
+        $this->assertEquals($temperaturesInOrder, array_map('intval', array_column($content, 'temperature')));
+        $humiditiesInOrder = [30, 40, 50];
+        $this->assertEquals($humiditiesInOrder, array_map('intval', array_column($content, 'humidity')));
+    }
+    
+    public function testGettingElectricityMeasurementLogsAscending() {
+        $content = $this->testGettingMeasurementLogsAscending(4);
+        $this->testElectricityMeasurementLogsOrder($content, [854800,854900,855000]);
+    }
+
+    private function testGettingMeasurementLogsWithOffset(int $channelId) {
+        $client = $this->createAuthenticatedClient($this->user);
+        $client->apiRequestV22('GET', '/api/channels/'.$channelId.'/measurement-logs?offset=1&limit=1');
         $response = $client->getResponse();
         $this->assertStatusCode('2xx', $response);
         $content = json_decode($response->getContent(), true);
         $this->assertCount(1, $content);
+        return $content;
+    }
+        
+    public function testGettingTemperatureLogsWithOffset() {
+        $content = $this->testGettingMeasurementLogsWithOffset(2);
         $this->assertEquals(22, intval($content[0]['temperature']));
+    }
+    
+    public function testGettingTemperatureAndHumidityLogsWithOffset() {
+        $content = $this->testGettingMeasurementLogsWithOffset(3);
+        $this->assertEquals(22, intval($content[0]['temperature']));
+        $this->assertEquals(40, intval($content[0]['humidity']));
+    }
+    
+    public function testGettingElectricityMeasurementLogsWithOffset() {
+        $content = $this->testGettingMeasurementLogsWithOffset(4);
+        $this->testElectricityMeasurementLogsOrder($content, [854900]);
+    }
+    
+    private function testGettingMeasurementLogsWithTimestampRange(int $channelId) {
+        $afterDate = new \DateTime('2018-09-15');
+        $beforeDate = new \DateTime('2018-09-17');
+        $client = $this->createAuthenticatedClient($this->user);
+        $client->apiRequestV22('GET', '/api/channels/'
+                .$channelId.'/measurement-logs?afterTimestamp='.$afterDate->getTimestamp()
+                .'&beforeTimestamp='.$beforeDate->getTimestamp());
+        $response = $client->getResponse();
+        $this->assertStatusCode('2xx', $response);
+        $content = json_decode($response->getContent(), true);
+        $this->assertCount(1, $content);
+
+        return $content;
+    }
+    
+    public function testGettingTemperatureLogsWithTimestampRange() {
+        $content = $this->testGettingMeasurementLogsWithTimestampRange(2);
+        $this->assertEquals(22, intval($content[0]['temperature']));
+    }
+    
+    public function testGettingTemperatureAndHumidityLogsWithTimestampRange() {
+        $content = $this->testGettingMeasurementLogsWithTimestampRange(3);
+        $this->assertEquals(22, intval($content[0]['temperature']));
+        $this->assertEquals(40, intval($content[0]['humidity']));
+    }
+    
+    public function testGettingElectricityMeasurementLogsWithTimestampRange() {
+        $content = $this->testGettingMeasurementLogsWithTimestampRange(4);
+        $this->testElectricityMeasurementLogsOrder($content, [854900]);
     }
 
     public function testGettingMeasurementLogsOfUnsupportedChannel() {
