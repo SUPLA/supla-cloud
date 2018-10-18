f8d58faf6a08d97c6a3cc4c9be1cbd694edf4a01
Wojciech Frącz <fraczwojciech@gmail.com>
2018-10-18 07:51:50 +0200

    Add return declaration in PHPDOC for User#getLocale()

--- src/SuplaBundle/Entity/User.php
+++ src/SuplaBundle/Entity/User.php
@@ -627,6 +627,7 @@ class User implements AdvancedUserInterface, EncoderAwareInterface {
         $this->locale = $locale;
     }
 
+    /** @return string|null */
     public function getLocale() {
         return $this->locale;
     }
