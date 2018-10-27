6bf620ea1a1fbb681f8278d113aa2954e1ed93fe
Przemek Zygmunt <przemek@supla.org>
2018-10-27 16:27:32 +0200

    Add the ability to limit the supported channel functions by channel type

--- src/SuplaBundle/Model/ChannelParamsUpdater/RangeParamsUpdater.php
+++ src/SuplaBundle/Model/ChannelParamsUpdater/RangeParamsUpdater.php
@@ -4,10 +4,13 @@ namespace SuplaBundle\Model\ChannelParamsUpdater;
 use Assert\Assertion;
 use SuplaBundle\Entity\IODeviceChannel;
 use SuplaBundle\Enums\ChannelFunction;
+use SuplaBundle\Enums\ChannelType;
 
 abstract class RangeParamsUpdater implements SingleChannelParamsUpdater {
     /** @var ChannelFunction[] */
     private $supportedFunctions;
+    /** @var ChannelType[] */
+    private $onlyTypes;
     /** @var int */
     private $min;
     /** @var int */
@@ -21,13 +24,15 @@ abstract class RangeParamsUpdater implements SingleChannelParamsUpdater {
      * @param int $min
      * @param int $max
      * @param int $paramNo
+     * @param null|ChannelType|ChannelType[] $onlyTypes
      */
-    public function __construct($supportedFunctions, int $min, int $max, int $paramNo = 1) {
+    public function __construct($supportedFunctions, int $min, int $max, int $paramNo = 1, $onlyTypes = null) {
         Assertion::inArray($paramNo, [1, 2, 3]);
         $this->supportedFunctions = is_array($supportedFunctions) ? $supportedFunctions : [$supportedFunctions];
         $this->min = $min;
         $this->max = $max;
         $this->paramNo = $paramNo;
+        $this->onlyTypes = $onlyTypes !== null ? (is_array($onlyTypes) ? $onlyTypes : [$onlyTypes]) : null;
     }
 
     public function updateChannelParams(IODeviceChannel $channel, IODeviceChannel $updatedChannel) {
@@ -38,6 +43,8 @@ abstract class RangeParamsUpdater implements SingleChannelParamsUpdater {
     }
 
     public function supports(IODeviceChannel $channel): bool {
-        return in_array($channel->getFunction(), $this->supportedFunctions);
+
+        return in_array($channel->getFunction(), $this->supportedFunctions)
+            && ($this->onlyTypes === null || in_array($channel->getType(), $this->onlyTypes));
     }
 }
