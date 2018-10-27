ccd32321455be31aa911e4231f7ffca38aa6abf5
Przemek Zygmunt <przemek@supla.org>
2018-10-27 16:29:11 +0200

    New "updaters" for params. UnitPrice and ImpulsesPerUnit.

---
+++ src/SuplaBundle/Model/ChannelParamsUpdater/AnyMeterUnitPrice.php
@@ -0,0 +1,17 @@
+<?php
+namespace SuplaBundle\Model\ChannelParamsUpdater;
+
+use SuplaBundle\Enums\ChannelFunction;
+
+class AnyMeterUnitPrice extends RangeParamsUpdater {
+    const MIN_PRICE = 0;
+    const MAX_PRICE = 1000;
+
+    public function __construct() {
+        parent::__construct(
+            [ChannelFunction::ELECTRICITYMETER(),
+                ChannelFunction::GASMETER(),
+                ChannelFunction::WATERMETER()],
+            self::MIN_PRICE * 100, self::MAX_PRICE * 100, 2);
+    }
+}
