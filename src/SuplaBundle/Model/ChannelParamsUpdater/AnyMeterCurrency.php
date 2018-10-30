76a2d2d71ad4da2ec1a37e516831271ab7b1ecbc
Przemek Zygmunt <przemek@supla.org>
2018-10-30 12:53:02 +0100

    Allow to assign an empty value

--- src/SuplaBundle/Model/ChannelParamsUpdater/AnyMeterCurrency.php
+++ src/SuplaBundle/Model/ChannelParamsUpdater/AnyMeterCurrency.php
@@ -7,7 +7,8 @@ use SuplaBundle\Entity\IODeviceChannel;
 class AnyMeterCurrency implements SingleChannelParamsUpdater {
 
     public function updateChannelParams(IODeviceChannel $channel, IODeviceChannel $updatedChannel) {
-        if (preg_match('/^[A-Z]{3}$/', $updatedChannel->getTextParam1())) {
+        if (!$updatedChannel->getTextParam1()
+            || preg_match('/^[A-Z]{3}$/', $updatedChannel->getTextParam1())) {
             $channel->setTextParam1($updatedChannel->getTextParam1());
         }
 
