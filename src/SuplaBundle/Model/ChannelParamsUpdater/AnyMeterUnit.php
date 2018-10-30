5e42d4c88508501d0451e5cab4234dfc133b422a
Przemek Zygmunt <przemek@supla.org>
2018-10-30 13:09:03 +0100

    ParamsUpdated - unit check function moved to the separated class.

--- src/SuplaBundle/Model/ChannelParamsUpdater/AnyMeterUnit.php
+++ src/SuplaBundle/Model/ChannelParamsUpdater/AnyMeterUnit.php
@@ -4,12 +4,11 @@ namespace SuplaBundle\Model\ChannelParamsUpdater;
 use SuplaBundle\Enums\ChannelFunction;
 use SuplaBundle\Entity\IODeviceChannel;
 
-class AnyMeterCurrency implements SingleChannelParamsUpdater {
+class AnyMeterUnit implements SingleChannelParamsUpdater {
 
     public function updateChannelParams(IODeviceChannel $channel, IODeviceChannel $updatedChannel) {
-        if (!$updatedChannel->getTextParam1()
-            || preg_match('/^[A-Z]{3}$/', $updatedChannel->getTextParam1())) {
-            $channel->setTextParam1($updatedChannel->getTextParam1());
+        if (strlen($updatedChannel->getTextParam2() <= 4)) {
+            $channel->setTextParam2($updatedChannel->getTextParam2());
         }
     }
 
