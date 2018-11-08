54f6ab8743ed4338802e5d7fc47dd1e6992b14f2
Przemek Zygmunt <przemek@supla.org>
2018-11-08 17:02:18 +0100

    AlexaEventGatewayCredentialsParamConverter added

---
+++ src/SuplaBundle/ParamConverter/AlexaEventGatewayCredentialsParamConverter.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace SuplaBundle\ParamConverter;
+
+use Assert\Assertion;
+use SuplaBundle\Model\CurrentUserAware;
+use SuplaBundle\Entity\AlexaEventGatewayCredentials;
+use SuplaBundle\Repository\AlexaEventGatewayCredentialsRepository;
+
+class AlexaEventGatewayCredentialsParamConverter extends AbstractBodyParamConverter {
+    use CurrentUserAware;
+
+    /** @var AlexaEventGatewayCredentialsRepository */
+    private $alexaEgcRepository;
+
+    public function getConvertedClass(): string {
+        return AlexaEventGatewayCredentials::class;
+    }
+
+    public function __construct(AlexaEventGatewayCredentialsRepository $alexaEgcRepository) {
+        $this->alexaEgcRepository = $alexaEgcRepository;
+    }
+
+    public function convert(array $requestData) {
+        $aegc = new AlexaEventGatewayCredentials($this->getCurrentUserOrThrow());
+
+        $accessToken = $requestData['aeg_access_token'];
+        $expiresIn =  intval($requestData['aeg_expires_in']);
+        $refreshToken = $requestData['aeg_refresh_token'];
+
+        Assertion::betweenLength($accessToken, 1, 255);
+        Assertion::betweenLength($refreshToken, 1, 255);
+
+        if ($expiresIn > 0) {
+            Assertion::max($expiresIn, 1000000000);
+            $interval = new \DateInterval('PT'.$expiresIn.'S');
+        } else {
+            $interval = new \DateInterval('P20Y');
+        }
+
+        $date = new \DateTime;
+        $expiresAt = $date->add($interval);
+
+        $aegc->setAccessToken($accessToken);
+        $aegc->setExpiresAt($expiresAt);
+        $aegc->setRefreshToken($refreshToken);
+
+        return $aegc;
+    }
+}
