1cdf1453433739e3f5f435798fb23ee01e7fd0a1
Przemek Zygmunt <przemek@supla.org>
2018-12-24 13:28:45 +0100

    Should be client_secret instead of secret

--- src/SuplaBundle/Model/TargetSuplaCloud.php
+++ src/SuplaBundle/Model/TargetSuplaCloud.php
@@ -34,7 +34,7 @@ class TargetSuplaCloud {
         } else {
             $inputData = $request->query->all();
         }
-        $inputData = array_merge($inputData, ['client_id' => $mappedClientData['mappedClientId'], 'secret' => $mappedClientData['secret']]);
+        $inputData = array_merge($inputData, ['client_id' => $mappedClientData['mappedClientId'], 'client_secret' => $mappedClientData['secret']]);
         return $this->sendRequest('/oauth/v2/token', $inputData);
     }
 
