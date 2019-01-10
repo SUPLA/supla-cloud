f68c84a5d6cb7b99de448a1d384e9a4100a1f08c
Przemysław Zygmunt <p.zygmunt@acsoftware.pl>
2019-01-10 11:24:24 +0100

    Electricity meter - impulses/unit

--- src/Frontend/src/channels/params/channel-params-electricity-meter.vue
+++ src/Frontend/src/channels/params/channel-params-electricity-meter.vue
@@ -9,15 +9,18 @@
             @change="$emit('change')"></channel-params-meter-unit>
         <channel-params-any-meter :channel="channel" :unit="unit"
             @change="$emit('change')"></channel-params-any-meter>
+        <channel-params-meter-impulses :channel="channel" :unit="unit"
+            @change="$emit('change')"></channel-params-meter-impulses>
     </dl>
 </template>
 
 <script>
     import ChannelParamsAnyMeter from "./channel-params-any-meter";
     import ChannelParamsMeterUnit from "./channel-params-meter-unit";
+    import ChannelParamsMeterImpulses from "./channel-params-meter-impulses";
 
     export default {
-        components: {ChannelParamsAnyMeter, ChannelParamsMeterUnit,},
+        components: {ChannelParamsAnyMeter, ChannelParamsMeterUnit, ChannelParamsMeterImpulses},
         props: ['channel'],
         data() {
             return {
