e92f07265e6518bb1097028ceb1f78a475e6de3f
Wojciech Frącz <fraczwojciech@gmail.com>
2019-02-01 21:28:23 +0100

    Move direct link execution result to Vue

--- src/Frontend/src/direct-links/direct-link-execution-result.vue
+++ src/Frontend/src/direct-links/direct-link-execution-result.vue
@@ -1,19 +1,48 @@
 <template>
     <div>
-        <div class="container text-center">
-            <h1 class="nocapitalize"
-                v-if="failureReason">{{ $t(failureReason) }}</h1>
-            <h1 class="nocapitalize"
-                v-else>{{ $t('Direct link has been executed') }}</h1>
-            <div v-if="failureReason == 'directLinkExecutionFailureReason_invalidActionParameters'">
-                <i class="pe-7s-edit"
-                    style="font-size: 160px"></i>
-                <p>{{ $t('This direct link requires further parameters in order to be executed properly. See the examples below to get an idea of what you might do.')}}</p>
-                <div style="font-size: 1.3em">
-                    <div v-if="action == 'SET_RGBW_PARAMETERS'">
-                        <div><code>{{currentUrl}}?brightness=40</code></div>
-                        <div><code>{{currentUrl}}?brightness=100</code></div>
+        <div class="container text-center"
+            v-if="directLink">
+            <div v-if="failureReason">
+                <h1 class="nocapitalize">{{ $t(failureReason) }}</h1>
+                <div v-if="failureReason == 'directLinkExecutionFailureReason_invalidActionParameters'">
+                    <i class="pe-7s-edit"
+                        style="font-size: 160px"></i>
+                    <p>{{ $t('This direct link requires further parameters in order to be executed properly. See the examples below to get an idea of what you might do.')}}</p>
+                    <div style="font-size: 1.3em">
+                        <div v-if="action == 'REVEAL_PARTIALLY'">
+                            <div><code>{{currentUrl}}?percentage=40</code></div>
+                            <div><code>{{currentUrl}}?percentage=60</code></div>
+                        </div>
+                        <div v-if="action == 'SET_RGBW_PARAMETERS'">
+                            <div v-if="['DIMMER', 'DIMMERANDRGBLIGHTING'].indexOf(directLink.subject.function.name) !== -1">
+                                <div><code>{{currentUrl}}?brightness=40</code></div>
+                                <div><code>{{currentUrl}}?brightness=100</code></div>
+                            </div>
+                            <div v-if="['RGBLIGHTING', 'DIMMERANDRGBLIGHTING'].indexOf(directLink.subject.function.name) !== -1">
+                                <div><code>{{currentUrl}}?color=0xFF6600</code></div>
+                                <div><code>{{currentUrl}}?color_brightness=40&color=16711935</code></div>
+                                <div><code>{{currentUrl}}?color_brightness=60&color=0x00FF00</code></div>
+                                <div><code>{{currentUrl}}?color=random</code></div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div v-else>
+                    <i class="pe-7s-close-circle"
+                        style="font-size: 160px"></i>
+                    <h5>{{ $t('Check the direct link you used and try again.') }}</h5>
+                </div>
+            </div>
+            <div v-else>
+                <div v-if="directLink.state">
+                    <function-icon :model="{functionId: directLink.subject.functionId, state: directLink.state}"
+                        width="100"></function-icon>
+                    <channel-state-table :single-state="directLink.state"></channel-state-table>
+                </div>
+                <div v-else>
+                    <h1 class="nocapitalize">{{ $t('Direct link has been executed') }}</h1>
+                    <i class="pe-7s-rocket"
+                        style="font-size: 160px"></i>
                 </div>
             </div>
         </div>
@@ -21,57 +50,25 @@
     </div>
 </template>
 
-<!--icon="pe-7s-edit">-->
-<!--<div style="font-size: 1.3em">-->
-<!--{% if action.name == 'REVEAL_PARTIALLY' %}-->
-<!--<div><code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?percentage=40</code></div>-->
-<!--<div><code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?percentage=60</code></div>-->
-<!--{% elseif action.name == 'SET_RGBW_PARAMETERS' %}-->
-<!--{% if directLink.subject.function.name in ['DIMMER', 'DIMMERANDRGBLIGHTING'] %}-->
-<!--<div><code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?brightness=40</code></div>-->
-<!--<div><code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?brightness=100</code></div>-->
-<!--{% endif %}-->
-<!--{% if directLink.subject.function.name in ['RGBLIGHTING', 'DIMMERANDRGBLIGHTING'] %}-->
-<!--<div><code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?color=0xFF6600</code></div>-->
-<!--<div>-->
-<!--<code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?color_brightness=40&color=16711935</code>-->
-<!--</div>-->
-<!--<div>-->
-<!--<code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?color_brightness=60&color=0x00FF00</code>-->
-<!--</div>-->
-<!--<div><code>{{ app.request.schemeAndHttpHost }}{{ app.request.pathinfo }}?color=random</code></div>-->
-<!--{% endif %}-->
-<!--{% endif %}-->
-<!--</div>-->
-<!--</error-page>-->
-<!--{% else %}-->
-<!--<error-page header-i18n="{{ failureReason.value }}"-->
-<!--message-i18n="Check the direct link you used and try again."-->
-<!--icon="pe-7s-close-circle">-->
-<!--</error-page>-->
-<!--{% endif %}-->
-<!--{% elseif details %}-->
-<!--<error-page>-->
-<!--<function-icon :model='{functionId: {{ directLink.subject.function.id }}, state: {{ details|json_encode|raw }}}'-->
-<!--width="100"></function-icon>-->
-<!--<channel-state-table :single-state='{{ details|json_encode|raw }}'></channel-state-table>-->
-<!--</error-page>-->
-<!--{% else %}-->
-<!--<error-page header-i18n="Direct link has been executed"-->
-<!--icon="pe-7s-rocket">-->
-<!--</error-page>-->
-<!--{% endif %}-->
-<!--</div>-->
-
 <script type="text/babel">
     import LoginFooter from "../login/login-footer";
+    import FunctionIcon from "../channels/function-icon";
+    import ChannelStateTable from "../channels/channel-state-table";
 
     export default {
         props: ['failureReason', 'action'],
-        components: {LoginFooter},
+        components: {ChannelStateTable, FunctionIcon, LoginFooter},
+        data() {
+            return {
+                directLink: undefined
+            };
+        },
+        mounted() {
+            this.directLink = window.directLink;
+        },
         computed: {
             currentUrl() {
-                return window.location.protocol + "://" + window.location.host + "/" + window.location.pathname;
+                return window.location.protocol + "//" + window.location.host + window.location.pathname;
             }
         }
     };
