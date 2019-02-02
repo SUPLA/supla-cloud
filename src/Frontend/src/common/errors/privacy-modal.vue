6dabe95b25fa56f0636363178dee2976b4cdc34c
Wojciech Frącz <fraczwojciech@gmail.com>
2019-02-02 10:23:57 +0100

    Fix setting the initial language in privacy policy modal

--- src/Frontend/src/common/errors/privacy-modal.vue
+++ src/Frontend/src/common/errors/privacy-modal.vue
@@ -1,8 +1,12 @@
 <template>
     <modal
         class="text-left modal-800 display-newlines">
-        <div slot
-            v-html="rules"></div>
+        <div slot>
+            <loading-cover :loading="!rules">
+                <div v-if="rules"
+                    v-html="rules"></div>
+            </loading-cover>
+        </div>
         <div slot="footer">
         </div>
     </modal>
@@ -16,12 +20,23 @@
             };
         },
         mounted() {
-            let rulesLang = 'en';
-            if (['pl', 'it', 'es', 'fr', 'ru', 'pt', 'de'].includes(this.$i18n.locale)) {
-                rulesLang = this.$i18n.locale;
+            this.fetch();
+        },
+        methods: {
+            fetch() {
+                this.rules = '';
+                let rulesLang = 'en';
+                if (['pl', 'it', 'es', 'fr', 'ru', 'pt', 'de'].includes(this.$i18n.locale)) {
+                    rulesLang = this.$i18n.locale;
+                }
+                this.$http.get(`/privacy/privacy_${rulesLang}.html`).then(response => this.rules = response.body);
             }
-
-            this.$http.get(`/privacy/privacy_${rulesLang}.html`).then(response => this.rules = response.body);
+        },
+        watch: {
+            '$i18n.locale'() {
+                this.fetch();
+            }
         }
     };
 </script>
+l
