d5386e836d1e73473374c229ef0517b012b34bdd
Wojciech Frącz <fraczwojciech@gmail.com>
2019-03-25 21:43:20 +0100

    Link to API docs from My OAuth apps & PATs

--- src/Frontend/src/oauth/oauth-apps/my-oauth-apps-page.vue
+++ src/Frontend/src/oauth/oauth-apps/my-oauth-apps-page.vue
@@ -4,11 +4,11 @@
             <h5>{{ $t('If you are a developer who wants to provide additional features for SUPLA, create an OAuth application that users can authorize and use.') }}</h5>
             <div class="form-group">
                 <div class="btn-group">
-                    <!--<a href="https://github.com/SUPLA/supla-cloud/wiki/Integrations#oauth-apps"-->
-                    <!--class="btn btn-green">-->
-                    <!--{{ $t('Full documentation')}}: GitHub-->
-                    <!--</a>-->
-                    <a href="/api/docs.html"
+                    <a href="https://github.com/SUPLA/supla-cloud/wiki/Integrations#oauth-apps"
+                        class="btn btn-white">
+                        {{ $t('Full documentation')}} @ GitHub
+                    </a>
+                    <a :href="'/api/docs.html' | withBaseUrl"
                         target="_blank"
                         class="btn btn-white">
                         {{ $t('API documentation')}}
