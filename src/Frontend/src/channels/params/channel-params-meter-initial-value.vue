f1bfa952f85ba2c2f4982f1329dc81cfd0c4d5b6
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-04-09 13:49:18 +0200

    Impulse counter - initial value

---
+++ src/Frontend/src/channels/params/channel-params-meter-initial-value.vue
@@ -0,0 +1,35 @@
+<template>
+    <dl>
+        <dd>{{ $t('Initial value') }}</dd>
+        <dt>
+            <span class="input-group">
+                <input type="number"
+                    step="1"
+                    min="-1000000"
+                    max="1000000"
+                    class="form-control text-center"
+                    v-model="initial-value">
+                <span class="input-group-addon">
+                    {{ unit }}
+                </span>
+            </span>
+        </dt>
+    </dl>
+</template>
+
+<script>
+    export default {
+        props: ['channel', 'unit'],
+        computed: {
+            impulses: {
+                set(value) {
+                    this.channel.param1 = value;
+                    this.$emit('change');
+                },
+                get() {
+                    return this.channel.param1;
+                }
+            }
+        },
+    };
+</script>
--- src/Frontend/src/channels/params/channel-params-water-meter.vue
+++ src/Frontend/src/channels/params/channel-params-water-meter.vue
@@ -6,6 +6,8 @@
             @change="$emit('change')"></channel-params-any-meter>
         <channel-params-meter-impulses :channel="channel" :unit="unit"
             @change="$emit('change')"></channel-params-meter-impulses>
+        <channel-params-meter-initial-value :channel="channel" :unit="unit"
+            @change="$emit('change')"></channel-params-meter-initial-value>
     </dl>
 </template>
 
@@ -13,9 +15,11 @@
     import ChannelParamsAnyMeter from "./channel-params-any-meter";
     import ChannelParamsMeterUnit from "./channel-params-meter-unit";
     import ChannelParamsMeterImpulses from "./channel-params-meter-impulses";
+    import ChannelParamsMeterInitialValue from "./channel-params-meter-initial-value";
 
     export default {
-        components: {ChannelParamsAnyMeter, ChannelParamsMeterUnit, ChannelParamsMeterImpulses},
+        components: {ChannelParamsMeterInitialValue, ChannelParamsAnyMeter,
+            ChannelParamsMeterUnit, ChannelParamsMeterImpulses, ChannelParamsMeterInitialValue},
         props: ['channel'],
         data() {
             return {
