00619f4aafd683bc4546c473d923b4d0ce6f73ce
Wojciech Frącz <fraczwojciech@gmail.com>
2019-06-10 14:06:04 +0200

    Scenes add page (WIP)
    ref #228

--- src/Frontend/src/scenes/scene-operations-editor.vue
+++ src/Frontend/src/scenes/scene-operations-editor.vue
@@ -15,12 +15,28 @@
                         width="40"></function-icon>
                     {{ channelTitle(operation.subject) }}
                 </td>
+                <td>
+                    <channel-action-chooser :subject="operation.subject"
+                        v-model="operation.action"
+                        :possible-action-filter="possibleActionFilter(operation.subject)"></channel-action-chooser>
+                </td>
+                <td>
+                    <div class="input-group">
+                        <input type="number"
+                            min="0"
+                            class="form-control"
+                            maxlength="4"
+                            v-model="operation.delay">
+                        <span class="input-group-addon">s</span>
+                    </div>
+                </td>
             </tr>
             </tbody>
         </table>
         <div class="form-group">
             <label>{{ $t('Choose item to use in scene') }}</label>
-            <subject-dropdown @input="addSceneOperation($event)"></subject-dropdown>
+            <subject-dropdown @input="addSceneOperation($event)"
+                channelsDropdownParams="io=output"></subject-dropdown>
         </div>
     </div>
 </template>
@@ -29,10 +45,11 @@
     import SubjectDropdown from "../devices/subject-dropdown";
     import FunctionIcon from "../channels/function-icon";
     import {channelTitle} from "../common/filters";
+    import ChannelActionChooser from "../channels/action/channel-action-chooser";
 
     export default {
         props: ['scene'],
-        components: {FunctionIcon, SubjectDropdown},
+        components: {ChannelActionChooser, FunctionIcon, SubjectDropdown},
         data() {
             return {};
         },
@@ -43,10 +60,15 @@
         },
         methods: {
             addSceneOperation({subject, type}) {
-                this.scene.operations.push({subject, subjectType: type});
+                this.scene.operations.push({subject, subjectType: type, delay: 0});
             },
             channelTitle(subject) {
                 return channelTitle(subject, this, true);
+            },
+            possibleActionFilter(subject) {
+                return (possibleAction) =>
+                    (possibleAction.name != 'OPEN' || subject.function.possibleActions.length == 1)
+                    && possibleAction.name != 'CLOSE';
             }
         }
     };
