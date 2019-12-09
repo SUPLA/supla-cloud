087ebaf1de4678a79222087de7b372fa839f8beb
Wojciech Frącz <fraczwojciech@gmail.com>
2019-12-09 20:57:02 +0100

    Direct link filters in details page

--- src/Frontend/src/direct-links/direct-links-page.vue
+++ src/Frontend/src/direct-links/direct-links-page.vue
@@ -4,15 +4,18 @@
         endpoint="direct-links?include=subject"
         create-new-label-i18n="Create new direct link"
         :limit="$user.userData.limits.directLink"
+        filters="direct-link-filters"
         details-route="directLink"></carousel-page>
 </template>
 
 <script>
     import CarouselPage from "../common/pages/carousel-page";
     import DirectLinkTile from "./direct-link-tile";
+    import DirectLinkFilters from "./direct-link-filters";
     import Vue from "vue";
 
     Vue.component('DirectLinkTile', DirectLinkTile);
+    Vue.component('DirectLinkFilters', DirectLinkFilters);
 
     export default {
         components: {CarouselPage}
