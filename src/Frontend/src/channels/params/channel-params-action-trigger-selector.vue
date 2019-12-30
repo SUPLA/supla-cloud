db20b796a255360fb21a82b9b3c035703f1f4eb4
Wojciech Frącz <fraczwojciech@gmail.com>
2019-12-30 17:57:47 +0100

    Action trigger actions GUI: accordion

---
+++ src/Frontend/src/channels/params/channel-params-action-trigger-selector.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <subject-dropdown v-model="subject"
+            @input="onSubjectChange()"
+            channels-dropdown-params="io=output&hasFunction=1"></subject-dropdown>
+        <div v-if="subject">
+            <channel-action-chooser :subject="subject"
+                @input="onActionChange()"
+                v-model="action"></channel-action-chooser>
+        </div>
+        <button v-if="value"
+            type="button"
+            class="btn btn-default btn-block"
+            @click="$emit('input')">
+            <i class="pe-7s-close"></i>
+            {{ $t('Clear') }}
+        </button>
+    </div>
+</template>
+
+<script>
+    import SubjectDropdown from "../../devices/subject-dropdown";
+    import ChannelActionChooser from "../action/channel-action-chooser";
+    import changeCase from "change-case";
+
+    export default {
+        components: {ChannelActionChooser, SubjectDropdown},
+        props: ['value'],
+        data() {
+            return {
+                subject: undefined,
+                action: undefined,
+            };
+        },
+        mounted() {
+            this.onValueChanged();
+        },
+        watch: {
+            value() {
+                this.onValueChanged();
+            }
+        },
+        methods: {
+            onValueChanged() {
+                if (this.value && this.value.subjectType) {
+                    const endpoint = `${changeCase.paramCase(this.value.subjectType)}s/${this.value.subjectId}`;
+                    this.$http.get(endpoint)
+                        .then(response => this.subject = response.body)
+                        .then(() => this.action = this.value.action);
+                } else {
+                    this.subject = undefined;
+                    this.action = undefined;
+                }
+            },
+            onSubjectChange() {
+                this.action = undefined;
+            },
+            onActionChange() {
+                this.$emit('input', {
+                    subjectId: this.subject.id,
+                    subjectType: this.subject.subjectType,
+                    action: this.action,
+                });
+            },
+        }
+    };
+</script>
--- src/Frontend/src/channels/params/channel-params-action-trigger.vue
+++ src/Frontend/src/channels/params/channel-params-action-trigger.vue
@@ -1,73 +1,49 @@
 <template>
     <div>
-        <dl>
-            <dd>{{ $t('On trigger') }}</dd>
-            <dt>
-                <div class="">
-                    <div class="dropdown hovered">
-                        <button class="btn btn-default dropdown-toggle btn-block btn-wrapped"
-                            type="button"
-                            data-toggle="dropdown">
-                            <h4>
-                                <span v-if="selectedBehavior">{{ $t('actionTriggerBehavior_' + selectedBehavior) }}</span>
-                                <span v-else>{{ $t('Select trigger') }}</span>
-                            </h4>
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li v-for="behavior in channel.config.supportedBehaviors">
-                                <a @click="onBehaviorChange(behavior)"
-                                    v-show="behavior !== selectedBehavior">{{ $t('actionTriggerBehavior_' + behavior) }}</a>
-                            </li>
-                        </ul>
+        <div class="panel-group hovered"
+            role="tablist"
+            aria-multiselectable="true">
+            <div :class="'panel panel-' + (channel.config.actions[behavior] ? 'success' : 'default')"
+                v-for="behavior in channel.config.supportedBehaviors">
+                <div class="panel-heading"
+                    role="tab"
+                    :id="'heading' + behavior">
+                    <div class="panel-title"
+                        @click="toggleExpand(behavior)">
+                        <a role="button"
+                            :aria-expanded="!!expanded[behavior]"
+                            :aria-controls="'collapse' + behavior">
+                            {{ $t('actionTriggerBehavior_' + behavior) }}
+                        </a>
                     </div>
                 </div>
-            </dt>
-        </dl>
-        <dl v-if="selectedBehavior">
-            <dd>{{ $t('Do / execute') }}</dd>
-            <dt>
-                <subject-dropdown v-model="behaviorSubject"
-                    @input="onBehaviorSubjectChange()"
-                    channels-dropdown-params="io=output&hasFunction=1"></subject-dropdown>
-                <div v-if="behaviorSubject">
-                    <channel-action-chooser :subject="behaviorSubject"
-                        @input="onBehaviorActionChange()"
-                        v-model="behaviorAction"></channel-action-chooser>
+                <div class="panel-collapse"
+                    :id="'collapse' + behavior"
+                    role="tabpanel"
+                    :aria-labelledby="'heading' + behavior">
+                    <transition-expand>
+                        <div class="panel-body"
+                            v-if="expanded[behavior]">
+                            <channel-params-action-trigger-selector v-model="channel.config.actions[behavior]"
+                                @input="$emit('change')"></channel-params-action-trigger-selector>
+                        </div>
+                    </transition-expand>
                 </div>
-            </dt>
-        </dl>
-
-
-        <!--        <div class="form-group">-->
-        <!--            <label>{{ $t('Action when pressed (ON)' )}}</label>-->
-
-        <!--        </div>-->
-        <!--        <div class="form-group">-->
-        <!--            <label>{{ $t('Action when released (OFF)' )}}</label>-->
-        <!--            <subject-dropdown v-model="subject2WithType"-->
-        <!--                channels-dropdown-params="io=output&hasFunction=1"></subject-dropdown>-->
-        <!--            <div v-if="subject2WithType">-->
-        <!--                <channel-action-chooser :subject="subject2WithType.subject"-->
-        <!--                    v-model="action2"></channel-action-chooser>-->
-        <!--            </div>-->
-        <!--        </div>-->
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import SubjectDropdown from "../../devices/subject-dropdown";
-    import ChannelActionChooser from "../action/channel-action-chooser";
-    import changeCase from "change-case";
+    import TransitionExpand from "../../common/gui/transition-expand";
+    import ChannelParamsActionTriggerSelector from "./channel-params-action-trigger-selector";
 
     export default {
-        components: {ChannelActionChooser, SubjectDropdown},
+        components: {ChannelParamsActionTriggerSelector, TransitionExpand},
         props: ['channel'],
         data() {
             return {
-                selectedBehavior: undefined,
-                behaviorSubject: undefined,
-                behaviorAction: undefined
+                expanded: {}
             };
         },
         mounted() {
@@ -76,28 +52,15 @@
             }
         },
         methods: {
-            onBehaviorChange(behavior) {
-                this.selectedBehavior = behavior;
-                this.behaviorSubject = undefined;
-                this.behaviorAction = undefined;
-                const currentAction = this.channel.config.actions[behavior];
-                if (currentAction) {
-                    const endpoint = `${changeCase.paramCase(currentAction.subjectType)}s/${currentAction.subjectId}`;
-                    this.$http.get(endpoint)
-                        .then(response => this.behaviorSubject = response.body)
-                        .then(() => this.behaviorAction = currentAction.action);
-                }
-            },
-            onBehaviorSubjectChange() {
-                this.behaviorAction = undefined;
-            },
-            onBehaviorActionChange() {
-                this.channel.config.actions[this.selectedBehavior] = {
-                    subjectId: this.behaviorSubject.id,
-                    subjectType: this.behaviorSubject.subjectType,
-                    action: this.behaviorAction,
-                };
+            toggleExpand(behavior) {
+                this.$set(this.expanded, behavior, !this.expanded[behavior]);
             }
         }
     };
 </script>
+
+<style>
+    .panel-group .panel-heading {
+        cursor: pointer;
+    }
+</style>
