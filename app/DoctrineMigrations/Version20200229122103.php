d264d715d18992d716e687999df0d0a69b2da7e6
Wojciech Frącz <fraczwojciech@gmail.com>
2020-02-29 23:04:34 +0100

    Convert all user-input text columns to utf8mb4

--- app/DoctrineMigrations/Version20200229122103.php
+++ app/DoctrineMigrations/Version20200229122103.php
@@ -7,8 +7,14 @@ namespace Supla\Migrations;
  */
 class Version20200229122103 extends NoWayBackMigration {
     public function migrate() {
-        $this->addSql('ALTER TABLE `supla_iodevice` CHANGE `name` `name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_accessid` CHANGE `caption` `caption` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
         $this->addSql('ALTER TABLE `supla_client` CHANGE `name` `name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `caption` `caption` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_direct_link` CHANGE `caption` `caption` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_iodevice` CHANGE `name` `name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `comment` `comment` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_dev_channel` CHANGE `caption` `caption` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `text_param1` `text_param1` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `text_param2` `text_param2` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `text_param3` `text_param3` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_dev_channel_group` CHANGE `caption` `caption` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_location` CHANGE `caption` `caption` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_schedule` CHANGE `caption` `caption` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
 
         $this->addSql('DROP PROCEDURE IF EXISTS `supla_add_client`');
         $this->addSql(<<<PROCEDURE
