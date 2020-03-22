33e2ecbec770a53dfd8f087b7aa4f900ec520b5c
Wojciech Frącz <fraczwojciech@gmail.com>
2020-03-22 15:09:07 +0100

    Global API rate limit

--- src/SuplaBundle/EventListener/ApiRateLimit/ApiRateLimitListener.php
+++ src/SuplaBundle/EventListener/ApiRateLimit/ApiRateLimitListener.php
@@ -18,12 +18,16 @@
 namespace SuplaBundle\EventListener\ApiRateLimit;
 
 use Psr\Cache\CacheItemPoolInterface;
-use SuplaBundle\Model\Audit\AuditAware;
+use SuplaBundle\Model\CurrentUserAware;
+use SuplaBundle\Model\TimeProvider;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class ApiRateLimitListener {
-    use AuditAware;
+    use CurrentUserAware;
 
     /** @var CacheItemPoolInterface */
     private $cache;
@@ -31,21 +35,87 @@ class ApiRateLimitListener {
     private $enabled;
     /** @var string */
     private $globalLimit;
+    /** @var GlobalApiRateLimit */
+    private $globalApiRateLimit;
+    /** @var TimeProvider */
+    private $timeProvider;
 
-    public function __construct(bool $enabled, string $globalLimit, CacheItemPoolInterface $cache) {
+    /** @var ApiRateLimitStatus */
+    private $rateLimit;
+
+    public function __construct(
+        bool $enabled,
+        GlobalApiRateLimit $globalApiRateLimit,
+        CacheItemPoolInterface $cache,
+        TimeProvider $timeProvider
+    ) {
         $this->cache = $cache;
-        $this->globalLimit = $globalLimit;
+        $this->globalApiRateLimit = $globalApiRateLimit;
         $this->enabled = $enabled;
+        $this->timeProvider = $timeProvider;
     }
 
     public function onKernelRequest(GetResponseEvent $event) {
-        $this->noop();
+        if (!$this->isRequestRateLimited($event)) {
+            return;
+        }
+        if ($this->incAndCheckGlobalRate()->isExceeded()) {
+            $response = new Response('API cannot respond right now. Wait a while before subsequent request.', Response::HTTP_TOO_MANY_REQUESTS);
+            $event->setResponse($response);
+            $event->stopPropagation();
+            return;
+        }
     }
 
     public function onKernelResponse(FilterResponseEvent $event) {
-        $this->noop();
+        if (!$this->isRequestRateLimited($event)) {
+            return;
+        }
     }
 
-    private function noop() {
+    private function isRequestRateLimited(KernelEvent $event): bool {
+        if (!$this->enabled) {
+            return false;
+        }
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return false;
+        }
+        if (substr($event->getRequest()->getRequestUri(), 0, 5) != "/api/") {
+            return true;
+        }
+        if (substr($event->getRequest()->getRequestUri(), 0, 8) != "/direct/") {
+            return true;
+        }
+        return false;
+    }
+
+    private function incAndCheckGlobalRate(): ApiRateLimitStatus {
+        return $this->incAndCheck('global', function () {
+            return $this->globalApiRateLimit;
+        });
+    }
+
+    private function incAndCheck(string $key, callable $ruleProducer): ApiRateLimitStatus {
+        $item = $this->cache->getItem($key);
+        if ($item->isHit()) {
+            $rateLimitStatus = new ApiRateLimitStatus($item->get());
+        } else {
+            $rateLimitStatus = ApiRateLimitStatus::fromRule($ruleProducer(), $this->timeProvider);
+            $item->expiresAt(new \DateTime('@' . $rateLimitStatus->getReset()));
+        }
+        $rateLimitStatus->decrement();
+        $item->set($rateLimitStatus->toString());
+        $this->cache->save($item);
+        return $rateLimitStatus;
+    }
+
+    private function setRateLimitHeaders(Response $response) {
+        if ($this->rateLimit) {
+            $response->headers->add([
+                'X-RateLimit-Limit' => $this->rateLimit->getLimit(),
+                'X-RateLimit-Remaining' => $this->rateLimit->getRemaining(),
+                'X-RateLimit-Reset' => $this->rateLimit->getReset(),
+            ]);
+        }
     }
 }
---
+++ src/SuplaBundle/EventListener/ApiRateLimit/ApiRateLimitStatus.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SuplaBundle\EventListener\ApiRateLimit;
+
+use SuplaBundle\Model\TimeProvider;
+
+class ApiRateLimitStatus {
+    private $limit;
+    private $remaining;
+    private $reset;
+
+    public function __construct(string $status) {
+        $status = explode(',', $status);
+        $this->limit = intval($status[0]);
+        $this->remaining = intval($status[1] ?? 0);
+        $this->reset = intval($status[2] ?? 0);
+    }
+
+    public function getLimit(): int {
+        return $this->limit;
+    }
+
+    public function getRemaining(): int {
+        return $this->remaining;
+    }
+
+    public function getReset(): int {
+        return $this->reset;
+    }
+
+    public function decrement() {
+        --$this->limit;
+    }
+
+    public function isExceeded(): bool {
+        return $this->remaining <= 0;
+    }
+
+    public function toString() {
+        return "$this->limit.$this->remaining.$this->reset";
+    }
+
+    public function __toString() {
+        return $this->toString();
+    }
+
+    public static function fromRule(ApiRateLimitRule $rule, TimeProvider $timeProvider): self {
+        $limit = $rule->getLimit();
+        $remaining = $rule->getLimit();
+        $reset = $timeProvider->getTimestamp() + $rule->getPeriod();
+        return new self("$limit,$remaining,$reset");
+    }
+}
