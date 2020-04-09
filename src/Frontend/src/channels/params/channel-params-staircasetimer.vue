c9201d1e4d91eb92609a0c9b68be54d0606c6726
Wojciech Frącz <fraczwojciech@gmail.com>
2020-04-09 12:10:58 +0200

    Do not show time settings if 0x00100000 flag is present for staircasetimer

--- src/Frontend/src/channels/params/channel-params-staircasetimer.vue
+++ src/Frontend/src/channels/params/channel-params-staircasetimer.vue
@@ -1,20 +1,22 @@
 <template>
-    <dl>
-        <dd>{{ $t('Relay switching time') }}</dd>
-        <dt>
-            <span class="input-group">
-                <input type="number"
-                    step="0.5"
-                    min="0.5"
-                    max="3600"
-                    class="form-control text-center"
-                    v-model="param1">
-                <span class="input-group-addon">
-                    {{ $t('sec.') }}
+    <div>
+        <dl v-if="timeSelectorEnabled">
+            <dd>{{ $t('Relay switching time') }}</dd>
+            <dt>
+                <span class="input-group">
+                    <input type="number"
+                        step="0.5"
+                        min="0.5"
+                        max="3600"
+                        class="form-control text-center"
+                        v-model="param1">
+                    <span class="input-group-addon">
+                        {{ $t('sec.') }}
+                    </span>
                 </span>
-            </span>
-        </dt>
-    </dl>
+            </dt>
+        </dl>
+    </div>
 </template>
 
 <script>
@@ -33,7 +35,10 @@
                 get() {
                     return this.channel.param1 / 10;
                 }
-            }
+            },
+            timeSelectorEnabled() {
+                return this.channel && !(this.channel.flags & 0x00100000);
+            },
         }
     };
 </script>
