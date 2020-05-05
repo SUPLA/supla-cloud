34d1a8ccba3368323d434ebe19dca6aa2d4211fa
Wojciech Frącz <fraczwojciech@gmail.com>
2020-05-05 18:32:31 +0200

    Add API Rate Limit test to ApiInfoCommand

--- src/SuplaBundle/Command/User/ApiInfoCommand.php
+++ src/SuplaBundle/Command/User/ApiInfoCommand.php
@@ -58,7 +58,8 @@ class ApiInfoCommand extends ContainerAwareCommand {
         $this
             ->setName('supla:api:info')
             ->setDescription('Displays API info.')
-            ->addOption('users', 'u', InputOption::VALUE_NONE);
+            ->addOption('users', 'u', InputOption::VALUE_NONE)
+            ->addOption('test', 't', InputOption::VALUE_NONE);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
@@ -70,9 +71,9 @@ class ApiInfoCommand extends ContainerAwareCommand {
             $this->displayGlobalLimitStatus($io);
             if ($input->getOption('users')) {
                 $this->displayUsersLimitStatus($io);
-            } else {
-                $io->newLine();
-                $io->note('To display users limits, add -u');
+            }
+            if ($input->getOption('test')) {
+                $this->testLimit($io);
             }
         }
     }
@@ -95,6 +96,7 @@ class ApiInfoCommand extends ContainerAwareCommand {
         $io->section('Users Limits');
         $userClass = User::class;
         $users = $this->entityManager->createQuery("SELECT u FROM $userClass u")->iterate();
+        $someDisplayed = false;
         foreach ($users as $userRow) {
             /** @var User $user */
             $user = $userRow[0];
@@ -102,15 +104,24 @@ class ApiInfoCommand extends ContainerAwareCommand {
             if ($cacheItem->isHit()) {
                 $status = new ApiRateLimitStatus($cacheItem->get());
                 if ($status->getRemaining() != $status->getLimit() && $this->timeProvider->getTimestamp() < $status->getReset()) {
-                    $info = $status->getRemaining() . ' / ' . $status->getLimit();
-                    if ($status->isExceeded()) {
-                        $info .= ' (excess: ' . $status->getExcess() . ')';
-                    }
-                    $info .= ' (reset: ' . $this->resetInfo($status) . ')';
+                    $info = $this->statusInfo($status);
                     $io->writeln($user->getUsername() . ': ' . $info);
+                    $someDisplayed = true;
                 }
             }
         }
+        if (!$someDisplayed) {
+            $io->note('No users use API currently.');
+        }
+    }
+
+    private function statusInfo(ApiRateLimitStatus $status): string {
+        $info = $status->getRemaining() . ' / ' . $status->getLimit();
+        if ($status->isExceeded()) {
+            $info .= ' (excess: ' . $status->getExcess() . ')';
+        }
+        $info .= ' (reset: ' . $this->resetInfo($status) . ')';
+        return $info;
     }
 
     private function resetInfo(ApiRateLimitStatus $globalRateLimitStatus) {
@@ -118,5 +129,37 @@ class ApiInfoCommand extends ContainerAwareCommand {
             . ' ('
             . ((new \DateTime('@' . $globalRateLimitStatus->getReset()))->format(\DateTime::ATOM))
             . ')';
+    }
+
+    private function testLimit(SymfonyStyle $io) {
+        $io->section('API Rate Limit test for hypothetical user (ID: -1)');
+        $rateLimitStatus = $this->getTestUserLimitStatus();
+        $io->writeln('Limit after got from storage: ' . $this->statusInfo($rateLimitStatus));
+        $rateLimitStatus->decrement();
+        $io->writeln('Limit after request:          ' . $this->statusInfo($rateLimitStatus));
+        $item = $this->apiRateLimitStorage->getItem($this->apiRateLimitStorage->getUserKey('-1'));
+        $item->set($rateLimitStatus->toString());
+        $item->expiresAfter(1209600); // if not checked in 14 days, expire
+        $this->apiRateLimitStorage->save($item);
+        $io->write('Waiting 2s...');
+        sleep(2);
+        $io->write("\r");
+        $rateLimitStatus = $this->getTestUserLimitStatus();
+        $io->writeln('Limit after got from storage: ' . $this->statusInfo($rateLimitStatus));
+    }
+
+    private function getTestUserLimitStatus(): ApiRateLimitStatus {
+        $item = $this->apiRateLimitStorage->getItem($this->apiRateLimitStorage->getUserKey('-1'));
+        $rateLimitStatus = null;
+        if ($item->isHit()) {
+            $rateLimitStatus = new ApiRateLimitStatus($item->get());
+            if ($rateLimitStatus->isExpired($this->timeProvider)) {
+                $rateLimitStatus = null;
+            }
+        }
+        if (!$rateLimitStatus) {
+            $rateLimitStatus = ApiRateLimitStatus::fromRule($this->defaultUserApiRateLimit, $this->timeProvider);
+        }
+        return $rateLimitStatus;
     }
 }
