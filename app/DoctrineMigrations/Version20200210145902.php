6a708351fa8a92c26f6cae9286c353bc0b888552
Wojciech Frącz <fraczwojciech@gmail.com>
2020-05-20 09:19:33 +0200

    Backport some migrations from v2.4 to v2.3

---
+++ app/DoctrineMigrations/Version20200210145902.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Supla\Migrations;
+
+/**
+ * The 'on' parameter has been added to the supla_add_thermostat_log_item procedure
+ */
+class Version20200210145902 extends NoWayBackMigration {
+    public function migrate() {
+        $this->addSql('DROP PROCEDURE IF EXISTS `supla_add_thermostat_log_item`');
+        $this->addSql('CREATE PROCEDURE `supla_add_thermostat_log_item`(IN `_channel_id` INT(11), IN `_measured_temperature` DECIMAL(5,2), IN `_preset_temperature` DECIMAL(5,2), IN `_on` TINYINT) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER BEGIN INSERT INTO `supla_thermostat_log`(`channel_id`, `date`, `measured_temperature`, `preset_temperature`, `on`) VALUES (_channel_id,UTC_TIMESTAMP(),_measured_temperature, _preset_temperature, _on); END');
+    }
+}
