b7ebb7b0b50b93eba23124ec6512bea094f909b4
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-07-24 15:58:46 +0200

    Class name fix

--- app/DoctrineMigrations/Version20200724155001.php
+++ app/DoctrineMigrations/Version20200724155001.php
@@ -5,7 +5,7 @@ namespace Supla\Migrations;
 /**
  * Time calculation method corrected. Should correctly be calculated based on UTC_TIMESTAMP instead of NOW ()
  */
-class Version20200123235701 extends NoWayBackMigration {
+class Version20200724155001 extends NoWayBackMigration {
     public function migrate() {
         $this->addSql('DROP PROCEDURE IF EXISTS `supla_set_registration_enabled`');
         $this->addSql("CREATE PROCEDURE `supla_set_registration_enabled`(IN `user_id` INT, IN `iodevice_sec` INT, IN `client_sec` INT) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER BEGIN IF iodevice_sec >= 0 THEN SET @date = NULL; IF iodevice_sec > 0 THEN SET @date = DATE_ADD(UTC_TIMESTAMP, INTERVAL iodevice_sec SECOND); END IF; UPDATE supla_user SET iodevice_reg_enabled = @date WHERE id = user_id; END IF; IF client_sec >= 0 THEN SET @date = NULL; IF client_sec > 0 THEN SET @date = DATE_ADD(UTC_TIMESTAMP, INTERVAL client_sec SECOND); END IF; UPDATE supla_user SET client_reg_enabled = @date WHERE id = user_id; END IF; END");
