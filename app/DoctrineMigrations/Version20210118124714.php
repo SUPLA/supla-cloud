6933f6fc90d8605b9a0869476e84e8e3e2b7fbd6
Wojciech Frącz <fraczwojciech@gmail.com>
2021-01-22 11:32:50 +0100

    Fix supla_mqtt_broker_auth procedure

--- app/DoctrineMigrations/Version20210118124714.php
+++ app/DoctrineMigrations/Version20210118124714.php
@@ -11,20 +11,17 @@ class Version20210118124714 extends NoWayBackMigration {
         $this->addSql('ALTER TABLE supla_oauth_client_authorizations ADD mqtt_broker_auth_password VARCHAR(128) DEFAULT NULL');
         $this->addSql('DROP PROCEDURE IF EXISTS `supla_mqtt_broker_auth`');
         $this->addSql(<<<PROCEDURE
-            CREATE PROCEDURE `supla_mqtt_broker_auth`(IN `in_suid` VARCHAR(255) CHARSET utf8, IN `in_password` VARCHAR(255) CHARSET utf8)
-            NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
+            CREATE  PROCEDURE `supla_mqtt_broker_auth`(IN `in_suid` VARCHAR(255) CHARSET utf8, IN `in_password` VARCHAR(255) CHARSET utf8)
+            NO SQL
             BEGIN
-                DECLARE hashed_password CHAR(128);
-                SELECT SHA2(in_password COLLATE utf8_general_ci, 512) INTO hashed_password;
-                
-                SELECT 1 FROM supla_user su LEFT JOIN supla_oauth_client_authorizations soca ON su.id = soca.user_id 
-                WHERE mqtt_broker_enabled = 1 AND short_unique_id = BINARY in_suid COLLATE utf8_general_ci 
-                AND (
-                    su.mqtt_broker_auth_password = hashed_password OR
-                  soca.mqtt_broker_auth_password = hashed_password
-                )
-                LIMIT 1;
-            END
+                SET @hashed_password = SHA2(in_password, 512);
+            SELECT 1 FROM supla_user su
+            LEFT JOIN supla_oauth_client_authorizations soca ON su.id = soca.user_id
+            WHERE mqtt_broker_enabled = 1 AND short_unique_id = BINARY in_suid AND(
+                su.mqtt_broker_auth_password = @hashed_password OR soca.mqtt_broker_auth_password = @hashed_password
+            )
+            LIMIT 1;
+END
 PROCEDURE
         );
         $this->addSql('ALTER TABLE supla_oauth_access_tokens ADD api_client_authorization_id INT DEFAULT NULL');
