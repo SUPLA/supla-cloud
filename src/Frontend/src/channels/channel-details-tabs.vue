efc0362fdfe4d59847ce453ab9c87f1d36b212d9
Wojciech Frącz <fraczwojciech@gmail.com>
2021-01-27 02:06:36 +0100

    Small fixes for channel details tabs

--- src/Frontend/src/channels/channel-details-tabs.vue
+++ src/Frontend/src/channels/channel-details-tabs.vue
@@ -75,7 +75,8 @@
                     count: this.channel.relationsCount.directLinks,
                 });
             }
-            var supporterFunctions = ['THERMOMETER',
+            const measurementsHistoryFunctions = [
+                'THERMOMETER',
                 'HUMIDITY',
                 'HUMIDITYANDTEMPERATURE',
                 'ELECTRICITYMETER',
@@ -83,9 +84,9 @@
                 'WATERMETER',
                 'HEATMETER',
                 'THERMOSTAT',
-                'THERMOSTATHEATPOLHOMEPLUS'];
-
-            if (supporterFunctions.indexOf(this.channel.function.name) >= 0) {
+                'THERMOSTATHEATPOLHOMEPLUS'
+            ];
+            if (measurementsHistoryFunctions.includes(this.channel.function.name)) {
                 this.availableTabs.push({
                     id: 'measurementsHistory',
                     header: 'History of measurements', // i18n
