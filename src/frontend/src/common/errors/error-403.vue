8a709389f141906623330ffe0d31a1b3149ca975
Wojciech Frącz <fraczwojciech@gmail.com>
2021-06-14 14:01:03 +0200

    Better styles for icons on error pages

--- src/frontend/src/common/errors/error-403.vue
+++ src/frontend/src/common/errors/error-403.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container text-center">
         <h1 class="nocapitalize">{{ $t('Seems like a secret place') }}</h1>
-        <i class="pe-7s-door-lock"
+        <i class="pe-7s-door-lock error-page-icon"
             style="font-size: 160px"></i>
         <h5>{{ $t('The server has returned an HTTP 403 Error. It means that you are not authorized to display the item you are looking for.') }}</h5>
     </div>
--- src/frontend/src/common/errors/update-in-progress.vue
+++ src/frontend/src/common/errors/update-in-progress.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container text-center">
         <h1 class="nocapitalize">{{ $t('Update in progress') }}</h1>
-        <i class="pe-7s-tools"
+        <i class="pe-7s-tools error-page-icon"
             style="font-size: 160px"></i>
         <h4>{{ $t('After the update, the application will be faster, more comfortable, more powerful... Just better!') }}</h4>
         <h5>{{ $t('Be right back!') }}</h5>
