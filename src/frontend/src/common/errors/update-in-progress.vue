fe74b56f2fe7e8b489f63987e57c8850ed03488c
Wojciech Frącz <fraczwojciech@gmail.com>
2021-06-14 13:54:55 +0200

    Update in progress - translations (PL)

--- src/frontend/src/common/errors/update-in-progress.vue
+++ src/frontend/src/common/errors/update-in-progress.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container text-center">
-        <h1 class="nocapitalize">{{ $t('Feature update in progress') }}</h1>
+        <h1 class="nocapitalize">{{ $t('Update in progress') }}</h1>
         <i class="pe-7s-tools"
             style="font-size: 160px"></i>
         <h4>{{ $t('After the update, the application will be faster, more comfortable, more powerful... Just better!') }}</h4>
