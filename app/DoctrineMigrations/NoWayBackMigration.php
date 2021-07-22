4d88136d146290e9b4971a2c2926fc62323b7070
Wojciech Frącz <fraczwojciech@gmail.com>
2021-07-23 01:07:33 +0200

    Upgrade doctrine migrations v1.3.2 -> v2.2.2, provide migration template

--- app/DoctrineMigrations/NoWayBackMigration.php
+++ app/DoctrineMigrations/NoWayBackMigration.php
@@ -17,22 +17,23 @@
 
 namespace Supla\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 abstract class NoWayBackMigration extends AbstractMigration implements ContainerAwareInterface {
     use ContainerAwareTrait;
 
-    public final function up(Schema $schema) {
+    public final function up(Schema $schema): void {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on MySQL.');
         $this->migrate();
     }
 
     abstract public function migrate();
 
-    public function down(Schema $schema) {
+    public function down(Schema $schema): void {
         $this->abortIf(true, 'There is no way back.');
     }
 
@@ -40,7 +41,7 @@ abstract class NoWayBackMigration extends AbstractMigration implements Container
         return $this->getConnection()->fetchAll($sqlQuery);
     }
 
-    protected function getConnection(): \Doctrine\DBAL\Connection {
+    protected function getConnection(): Connection {
         return $this->container->get('doctrine.orm.entity_manager')->getConnection();
     }
 }
