1e11509909cdfb95f37bdc77a06ff049da442e86
Wojciech Frącz <fraczwojciech@gmail.com>
2021-09-21 16:12:35 +0200

    Update translations (remove AT description)

--- src/frontend/src/channels/action-trigger/action-trigger-panel.vue
+++ src/frontend/src/channels/action-trigger/action-trigger-panel.vue
@@ -26,7 +26,6 @@
                     <transition-expand>
                         <div class="panel-body"
                             v-if="expanded[trigger]">
-                            <p class="small text-muted">{{ $t('actionTriggerDescription_' + trigger) }}</p>
                             <action-trigger-single-action-selector v-model="channel.config.actions[trigger]"
                                 @input="$emit('change')"></action-trigger-single-action-selector>
                         </div>
@@ -35,19 +34,19 @@
             </div>
         </div>
     </div>
-    <!-- i18n:['actionTrigger_TURN_ON','actionTriggerDescription_TURN_ON'] -->
-    <!-- i18n:['actionTrigger_TURN_OFF','actionTriggerDescription_TURN_OFF'] -->
-    <!-- i18n:['actionTrigger_TOGGLE_X1','actionTriggerDescription_TOGGLE_X1'] -->
-    <!-- i18n:['actionTrigger_TOGGLE_X2','actionTriggerDescription_TOGGLE_X2'] -->
-    <!-- i18n:['actionTrigger_TOGGLE_X3','actionTriggerDescription_TOGGLE_X3'] -->
-    <!-- i18n:['actionTrigger_TOGGLE_X4','actionTriggerDescription_TOGGLE_X4'] -->
-    <!-- i18n:['actionTrigger_TOGGLE_X5','actionTriggerDescription_TOGGLE_X5'] -->
-    <!-- i18n:['actionTrigger_HOLD','actionTriggerDescription_HOLD'] -->
-    <!-- i18n:['actionTrigger_SHORT_PRESS_X1','actionTriggerDescription_SHORT_PRESS_X1'] -->
-    <!-- i18n:['actionTrigger_SHORT_PRESS_X2','actionTriggerDescription_SHORT_PRESS_X2'] -->
-    <!-- i18n:['actionTrigger_SHORT_PRESS_X3','actionTriggerDescription_SHORT_PRESS_X3'] -->
-    <!-- i18n:['actionTrigger_SHORT_PRESS_X4','actionTriggerDescription_SHORT_PRESS_X4'] -->
-    <!-- i18n:['actionTrigger_SHORT_PRESS_X5','actionTriggerDescription_SHORT_PRESS_X5'] -->
+    <!-- i18n:['actionTrigger_TURN_ON'] -->
+    <!-- i18n:['actionTrigger_TURN_OFF'] -->
+    <!-- i18n:['actionTrigger_TOGGLE_X1'] -->
+    <!-- i18n:['actionTrigger_TOGGLE_X2'] -->
+    <!-- i18n:['actionTrigger_TOGGLE_X3'] -->
+    <!-- i18n:['actionTrigger_TOGGLE_X4'] -->
+    <!-- i18n:['actionTrigger_TOGGLE_X5'] -->
+    <!-- i18n:['actionTrigger_HOLD'] -->
+    <!-- i18n:['actionTrigger_SHORT_PRESS_X1'] -->
+    <!-- i18n:['actionTrigger_SHORT_PRESS_X2'] -->
+    <!-- i18n:['actionTrigger_SHORT_PRESS_X3'] -->
+    <!-- i18n:['actionTrigger_SHORT_PRESS_X4'] -->
+    <!-- i18n:['actionTrigger_SHORT_PRESS_X5'] -->
 </template>
 
 <script>
