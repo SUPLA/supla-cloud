20c59c477229e38679b35acb2164076841cd393e
Wojciech Frącz <fraczwojciech@gmail.com>
2022-01-05 14:53:14 +0100

    Lint fix

--- src/SuplaBundle/EventListener/ApiExceptionHandler.php
+++ src/SuplaBundle/EventListener/ApiExceptionHandler.php
@@ -50,7 +50,7 @@ class ApiExceptionHandler implements EventSubscriberInterface {
         $errorResponse = $this->chooseErrorResponse($exception);
         if ($isApiRequest || in_array('application/json', $request->getAcceptableContentTypes())) {
             $event->setResponse($errorResponse);
-        } else if (!($exception instanceof HttpException)) {
+        } elseif (!($exception instanceof HttpException)) {
             $responseContent = json_decode($errorResponse->getContent(), true);
             $event->setThrowable(new ApiException($responseContent['message'], $responseContent['status']));
         }
