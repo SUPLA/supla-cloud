934bed2678fd30d00f2a950c69bc40323c298499
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-01-10 17:59:08 +0100

    The CHRSET should be set to utf8mb4

--- app/DoctrineMigrations/Version20211218174444.php
+++ app/DoctrineMigrations/Version20211218174444.php
@@ -24,7 +24,7 @@ class Version20211218174444 extends NoWayBackMigration {
     public function migrate() {
         $this->addSql('DROP PROCEDURE IF EXISTS `supla_add_client`');
         $this->addSql(<<<PROCEDURE
-CREATE PROCEDURE `supla_add_client` (IN `_access_id` INT(11), IN `_guid` VARBINARY(16), IN `_name` VARCHAR(100) CHARSET utf8, 
+CREATE PROCEDURE `supla_add_client` (IN `_access_id` INT(11), IN `_guid` VARBINARY(16), IN `_name` VARCHAR(100) CHARSET utf8mb4, 
 IN `_reg_ipv4` INT(10) UNSIGNED, IN `_software_version` VARCHAR(20) CHARSET utf8, IN `_protocol_version` INT(11), IN `_user_id` INT(11), 
 IN `_auth_key` VARCHAR(64) CHARSET utf8, OUT `_id` INT(11))  NO SQL
 BEGIN
