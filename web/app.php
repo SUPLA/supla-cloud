c143bd8eb6bdf8517e0e10d8905c517e12e3bcfe
Wojciech Frącz <fraczwojciech@gmail.com>
2022-02-06 19:41:51 +0100

    Log PHP errors to var/logs/php_error.log

--- web/app.php
+++ web/app.php
@@ -6,6 +6,9 @@ require __DIR__ . '/../app/autoload.php';
 
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?: 'prod'));
 
+ini_set('log_errors', 1);
+ini_set('error_log', AppKernel::VAR_PATH . '/logs/php_error.log');
+
 $kernel = new AppKernel(APPLICATION_ENV, APPLICATION_ENV === 'dev');
 if (APPLICATION_ENV === 'dev') {
     Symfony\Component\ErrorHandler\Debug::enable();
