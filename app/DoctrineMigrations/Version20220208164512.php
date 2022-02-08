1e7a8753329fd8c4640c362e6d14a5d38a7bbf94
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-02-09 00:24:11 +0100

    Allow adding new flags for a device and channels.

--- app/DoctrineMigrations/Version20220208164512.php
+++ app/DoctrineMigrations/Version20220208164512.php
@@ -23,12 +23,14 @@ use SuplaBundle\Enums\ChannelType;
 /**
  * New channel.config.addToHistory for EM and IC.
  * Split channel.config.numberOfAttemptsToOpenOrClose to numberOfAttemptsToOpen and numberOfAttemptsToClose.
+ * Allow adding new flags for a device and channels.
  */
 class Version20220208164512 extends NoWayBackMigration {
     public function migrate() {
         $this->updateImpulseCountersAddToHistory();
         $this->updateElectricityMetersAddToHistory();
         $this->migrateNumberOfAttemptsToOpenOrClose();
+        $this->migrateProcedures();
     }
 
     private function updateImpulseCountersAddToHistory() {
@@ -79,5 +81,40 @@ class Version20220208164512 extends NoWayBackMigration {
                 );
             }
         }
+    }
+
+    private function migrateProcedures() {
+        $this->addSql('DROP PROCEDURE IF EXISTS `supla_set_channel_flags`');
+        $this->addSql('DROP PROCEDURE IF EXISTS `supla_update_iodevice`');
+
+        $this->addSql(<<<PROCEDURE
+CREATE PROCEDURE `supla_set_channel_flags`(IN `_user_id` INT, IN `_channel_id` INT, IN `_flags` INT)
+    NO SQL
+UPDATE supla_dev_channel SET flags = flags | _flags WHERE id = _channel_id AND user_id = _user_id
+PROCEDURE
+        );
+
+        $this->addSql(<<<PROCEDURE
+CREATE PROCEDURE `supla_update_iodevice`(IN `_name` VARCHAR(100) CHARSET utf8mb4, IN `_last_ipv4` INT(10) UNSIGNED, 
+  IN `_software_version` VARCHAR(20) CHARSET utf8, IN `_protocol_version` INT(11), IN `_original_location_id` INT(11), 
+  IN `_auth_key` VARCHAR(64) CHARSET utf8, IN `_id` INT(11), IN `_flags` INT(11))
+    NO SQL
+BEGIN
+UPDATE `supla_iodevice`
+SET
+`name` = _name,
+`last_connected` = UTC_TIMESTAMP(),
+`last_ipv4` = _last_ipv4,
+`software_version` = _software_version,
+`protocol_version` = _protocol_version,
+original_location_id = _original_location_id,
+`flags` = `flags` | _flags WHERE `id` = _id;
+IF _auth_key IS NOT NULL THEN
+  UPDATE `supla_iodevice`
+  SET `auth_key` = _auth_key WHERE `id` = _id AND `auth_key` IS NULL;
+END IF;
+END
+PROCEDURE
+        );
     }
 }
