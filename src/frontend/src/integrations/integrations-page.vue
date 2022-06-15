2c6af89b76c9eb7c2196249016d97ba821609119
Wojciech Frącz <fraczwojciech@gmail.com>
2022-06-15 11:28:38 +0200

    Add link to /apps page
    fix #552

--- src/frontend/src/integrations/integrations-page.vue
+++ src/frontend/src/integrations/integrations-page.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
         <div class="container">
-            <h1 v-title>{{ $t('Integrations') }}</h1>
-            <h4>{{$t('Thanks to integrations, you can easily add new features to your account using solutions provided by external suppliers, developers or by yourself.')}}</h4>
+
+            <div class="clearfix left-right-header">
+                <div>
+                    <h1 v-title>{{ $t('Integrations') }}</h1>
+                    <h4>{{ $t('Thanks to integrations, you can easily add new features to your account using solutions provided by external suppliers, developers or by yourself.') }}</h4>
+                </div>
+                <div>
+                    <a href="https://cloud.supla.org/apps"
+                        class="btn btn-white mb-3 mt-4"
+                        target="_blank">{{ $t('See available integrations') }}</a>
+                </div>
+            </div>
+
             <div class="form-group">
                 <div class="btn-filters">
                     <div class="btn-group btn-group-filters">
