74c410582b50317558b97e30381f49e34eb8629e
Wojciech Frącz <fraczwojciech@gmail.com>
2022-07-18 19:20:00 +0200

    Fix USER-ON-MQTT-SETTINGS-CHANGED test

--- src/SuplaBundle/Tests/Integration/Auth/OAuthAuthenticationIntegrationTest.php
+++ src/SuplaBundle/Tests/Integration/Auth/OAuthAuthenticationIntegrationTest.php
@@ -395,6 +395,6 @@ class OAuthAuthenticationIntegrationTest extends IntegrationTestCase {
         $this->assertEquals('mqtt_broker', $response['scope']);
         $this->user = $this->getEntityManager()->find(User::class, $this->user->getId());
         $this->assertTrue($this->user->isMqttBrokerEnabled());
-        $this->assertContains('ON-USER-MQTT-SETTINGS-CHANGED:' . $this->user->getId(), SuplaServerMock::$executedCommands);
+        $this->assertContains('USER-ON-MQTT-SETTINGS-CHANGED:' . $this->user->getId(), SuplaServerMock::$executedCommands);
     }
 }
