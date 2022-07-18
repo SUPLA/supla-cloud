385a7fe40b2df2fe5d1b57fa980f02bdaa824f28
Wojciech Frącz <fraczwojciech@gmail.com>
2022-07-19 00:15:26 +0200

    Week schedule selector mobile improvements

--- src/frontend/src/access-ids/week-schedule-selector.vue
+++ src/frontend/src/access-ids/week-schedule-selector.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        <table class="week-schedule-selector"
+        <select class="form-control hidden visible-xs mb-3"
+            v-model="mobileWeekdayDisplay">
+            <option
+                v-for="(weekday, $index) in ['Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays', 'Sundays']"
+                :value="$index + 1"
+                :key="'mobileWeekday' + $index">
+                {{ $t(weekday) }}
+            </option>
+        </select>
+        <table :class="['week-schedule-selector', 'selection-mode-' + selectionMode, 'mobile-weekday-' + mobileWeekdayDisplay]"
             v-if="temporaryModel">
             <thead>
             <tr>
-                <th></th>
+                <th class="hour-header"></th>
                 <!-- i18n: ['Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays', 'Sundays'] -->
-                <th v-for="weekday in ['Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays', 'Sundays']"
+                <th v-for="(weekday, $index) in ['Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays', 'Sundays']"
                     :key="weekday"
-                    class="weekday-header ellipsis">
+                    :class="['hidden-xs weekday-header ellipsis', 'weekday-column-' + ($index + 1)]">
                     {{ $t(weekday) }}
                 </th>
             </tr>
@@ -18,12 +27,10 @@
                 :key="hour">
                 <th scope="row"
                     class="hour-header ellipsis">
-                    {{ hourLabelStart(hour) }}
-                    <span class="hidden-xs">
-                        {{ ' ' }} - {{ hourLabelEnd(hour) }}
-                    </span>
+                    {{ hourLabelStart(hour) }} - {{ hourLabelEnd(hour) }}
                 </th>
                 <td v-for="weekday in [1,2,3,4,5,6,7]"
+                    :class="['hidden-xs', 'weekday-column-' + weekday]"
                     :key="'0' + hour + weekday">
                     <a :class="['time-slot', {'green': temporaryModel[weekday][hour]}]"
                         @mousedown="startSelection(weekday, hour)"
@@ -38,12 +45,12 @@
             <tr>
                 <th></th>
                 <th v-for="weekday in [1,2,3,4,5,6,7]"
-                    class="copy-buttons text-center"
+                    :class="['hidden-xs text-center copy-buttons', 'weekday-column-' + weekday]"
                     :key="'copypaste' + weekday">
                     <a class="mx-1"
                         v-if="!copyFrom || copyFrom === weekday"
                         @click="copyFrom = (copyFrom === weekday ? undefined : weekday)">
-                        <span class="pe-7s-copy-file"></span>
+                        <span :class="['pe-7s-copy-file', {'copy-from': copyFrom === weekday}]"></span>
                     </a>
                     <a class="mx-1"
                         @click="copyDay(copyFrom, weekday)"
@@ -71,19 +78,27 @@
                 selectionMode: undefined,
                 selectionStartCoords: undefined,
                 copyFrom: undefined,
+                mouseUpCatcher: undefined,
+                mobileWeekdayDisplay: 1,
             };
         },
         mounted() {
             this.model = {};
+            const defaultValue = this.value && Object.keys(this.value).length ? 0 : 1;
             [...Array(7).keys()].forEach((weekday) => {
                 weekday += 1;
                 const hours = {};
                 [...Array(24).keys()].forEach((hour) => {
-                    hours[hour] = (this.value && this.value[weekday] && this.value[weekday][hour]) || 0;
+                    hours[hour] = (this.value && this.value[weekday] && this.value[weekday][hour]) || defaultValue;
                 });
                 this.$set(this.model, weekday, hours);
             });
             this.temporaryModel = cloneDeep(this.model);
+            this.mouseUpCatcher = () => this.finishSelection();
+            window.addEventListener('mouseup', this.mouseUpCatcher);
+        },
+        beforeDestroy() {
+            window.removeEventListener('mouseup', this.mouseUpCatcher);
         },
         methods: {
             hourLabelStart(hour) {
@@ -115,8 +130,10 @@
                 }
             },
             finishSelection() {
-                this.updateModel();
-                this.selectionStartCoords = undefined;
+                if (this.selectionStartCoords) {
+                    this.updateModel();
+                    this.selectionStartCoords = undefined;
+                }
             },
             updateModel() {
                 this.model = this.temporaryModel;
@@ -134,11 +151,17 @@
 
 <style lang="scss">
     @import '../styles/variables';
+    @import '../styles/mixins';
 
     table.week-schedule-selector {
         user-select: none;
         table-layout: fixed;
         width: 100%;
+
+        @include on-xs-and-down {
+            max-width: 90%;
+        }
+
         .weekday-header {
             text-align: center;
         }
@@ -182,6 +205,16 @@
             padding-top: .3em;
             a {
                 color: $supla-grey-dark;
+                .copy-from {
+                    font-weight: bold;
+                }
+            }
+        }
+        @for $i from 1 through 7 {
+            &.mobile-weekday-#{$i} {
+                .hidden-xs.weekday-column-#{$i} {
+                    display: table-cell !important;
+                }
             }
         }
     }
