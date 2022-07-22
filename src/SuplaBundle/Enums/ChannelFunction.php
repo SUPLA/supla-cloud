2bb7e30e67a93776deda636142fad42269dc5126
Wojciech Frącz <fraczwojciech@gmail.com>
2022-07-22 20:23:38 +0200

    Scene alternative icons

--- src/SuplaBundle/Enums/ChannelFunction.php
+++ src/SuplaBundle/Enums/ChannelFunction.php
@@ -364,7 +364,7 @@ final class ChannelFunction extends Enum {
             self::DIGIGLASS_VERTICAL => 1,
             self::DIGIGLASS_HORIZONTAL => 1,
             self::ELECTRICITYMETER => 1,
-            self::SCENE => 1,
+            self::SCENE => 5,
         ];
     }
 
