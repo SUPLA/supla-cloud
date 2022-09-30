8db8f17804e52e1dff75d71ef484f80a85fcb163
Wojciech Frącz <fraczwojciech@gmail.com>
2022-09-30 15:08:40 +0200

    Replace vue-timepicker with native HTML time component

--- src/frontend/src/schedules/schedule-form/modes/schedule-form-mode-daily-hour.vue
+++ src/frontend/src/schedules/schedule-form/modes/schedule-form-mode-daily-hour.vue
@@ -1,34 +1,14 @@
 <template>
     <div class="form-group">
-        <vue-timepicker v-model="time"
-            hide-clear-button
-            auto-scroll
-            advanced-keyboard
-            input-width="100%"
-            @change="updateTimeExpression()"></vue-timepicker>
+        <input type="time"
+            class="form-control"
+            v-model="theTime">
     </div>
 </template>
 
 <script>
-    import VueTimepicker from 'vue2-timepicker/src/vue-timepicker.vue';
-
     export default {
         props: ['weekdays', 'value'],
-        components: {VueTimepicker},
-        data() {
-            return {
-                time: '00:00',
-            };
-        },
-        mounted() {
-            if (this.value) {
-                const zeroPad = (num, places = 2) => String(num).padStart(places, '0')
-                const parts = this.value.split(' ');
-                this.time = `${zeroPad(parts[1])}:${zeroPad(parts[0])}`;
-            } else {
-                this.updateTimeExpression();
-            }
-        },
         methods: {
             updateTimeExpression() {
                 if (this.time) {
@@ -39,30 +19,38 @@
                     this.$emit('input', undefined);
                 }
             },
+            zeroPad(num, places = 2) {
+                return String(num).padStart(places, '0');
+            },
+        },
+        computed: {
+            theTime: {
+                set(value) {
+                    const timeParts = value.split(':');
+                    const cronExpression = [+timeParts[1], +timeParts[0], '*', '*', this.weekdays].join(' ');
+                    this.$emit('input', cronExpression);
+                },
+                get() {
+                    if (this.value) {
+                        const parts = this.value.split(' ');
+                        return `${this.zeroPad(parts[1])}:${this.zeroPad(parts[0])}`;
+                    } else {
+                        return '00:00';
+                    }
+                }
+            },
+        },
+        mounted() {
+            // this forces the cron expression to be calculated when the component starts
+            // eslint-disable-next-line no-self-assign
+            this.theTime = this.theTime;
         },
         watch: {
             weekdays() {
-                this.updateTimeExpression();
+                // this forces the cron expression to be recalculated
+                // eslint-disable-next-line no-self-assign
+                this.theTime = this.theTime;
             }
         }
     };
 </script>
-
-<style lang="scss">
-    @import '../../../styles/variables';
-
-    .time-picker {
-        .display-time {
-            border-radius: 5px;
-            text-align: center;
-            font-size: 1.3em !important;
-        }
-        .select-list {
-            ul li {
-                &.active {
-                    background: $supla-green !important;
-                }
-            }
-        }
-    }
-</style>
