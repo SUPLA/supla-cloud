de742e5517f804ec5f3db7af2c1110400239b00c
Wojciech Frącz <fraczwojciech@gmail.com>
2022-10-02 22:13:56 +0200

    Create user in AD when creating account with a supla:user:create command
    fix #614

--- src/SuplaBundle/Command/User/CreateConfirmedUserCommand.php
+++ src/SuplaBundle/Command/User/CreateConfirmedUserCommand.php
@@ -3,12 +3,13 @@ namespace SuplaBundle\Command\User;
 
 use SuplaBundle\Entity\User;
 use SuplaBundle\Model\UserManager;
+use SuplaBundle\Supla\SuplaAutodiscover;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 
 class CreateConfirmedUserCommand extends Command {
     const USERNAME_EXISTS_CODE = 123;
@@ -16,10 +17,13 @@ class CreateConfirmedUserCommand extends Command {
 
     /** @var UserManager */
     private $userManager;
+    /** @var SuplaAutodiscover */
+    private $autodiscover;
 
-    public function __construct(UserManager $userManager) {
+    public function __construct(UserManager $userManager, SuplaAutodiscover $autodiscover) {
         parent::__construct();
         $this->userManager = $userManager;
+        $this->autodiscover = $autodiscover;
     }
 
     protected function configure() {
@@ -28,7 +32,6 @@ class CreateConfirmedUserCommand extends Command {
             ->setAliases(['supla:create-confirmed-user'])
             ->addArgument('username', InputArgument::OPTIONAL)
             ->addArgument('password', InputArgument::OPTIONAL)
-            ->addOption('if-not-exists', null, InputOption::VALUE_NONE, 'Don’t throw an exception if no migration is available (CI).')
             ->setDescription('Create a confirmed user account.');
     }
 
@@ -38,16 +41,7 @@ class CreateConfirmedUserCommand extends Command {
         if (!$username) {
             $username = $helper->ask($input, $output, $this->usernameQuestion());
         } else {
-            try {
-                $username = $this->validateUsername($username);
-            } catch (\RuntimeException $e) {
-                if ($e->getCode() == self::USERNAME_EXISTS_CODE && $input->getOption('if-not-exists')) {
-                    $output->writeln("User $username already exists.");
-                    return;
-                } else {
-                    throw $e;
-                }
-            }
+            $username = $this->validateUsername($username);
         }
         $password = $input->getArgument('password');
         if (!$password) {
@@ -72,8 +66,10 @@ class CreateConfirmedUserCommand extends Command {
             throw new \RuntimeException('Invalid e-mail address');
         }
         $username = trim($username);
-        if ($this->userManager->userByEmail($username)) {
-            throw new \RuntimeException("User already exists! Choose different e-mail address.", self::USERNAME_EXISTS_CODE);
+        if ($this->autodiscover->userExists($username)) {
+            $targetCloud = $this->autodiscover->getAuthServerForUser($username);
+            $onServer = $targetCloud->isLocal() ? '' : " (on {$targetCloud->getHost()})";
+            throw new \RuntimeException("User already exists$onServer! Choose different e-mail address.", self::USERNAME_EXISTS_CODE);
         }
         return $username;
     }
@@ -96,6 +92,13 @@ class CreateConfirmedUserCommand extends Command {
     protected function createConfirmedUser(string $username, string $password) {
         $user = new User();
         $user->setEmail($username);
+        if ($this->autodiscover->enabled()) {
+            $createdInAd = $this->autodiscover->registerUser($user);
+            if (!$createdInAd) {
+                $message = "Error when communicating the AD. Try again in a while."; // i18n
+                throw new ServiceUnavailableHttpException(10, $message);
+            }
+        }
         $this->userManager->create($user);
         $this->userManager->setPassword($password, $user, true);
         $this->userManager->confirm($user->getToken());
---
+++ src/SuplaBundle/Tests/Integration/Command/CreateConfirmedUserCommandIntegrationTest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace SuplaBundle\Tests\Integration\Command;
+
+use SuplaBundle\Entity\User;
+use SuplaBundle\Supla\SuplaAutodiscoverMock;
+use SuplaBundle\Tests\Integration\IntegrationTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/** @small */
+class CreateConfirmedUserCommandIntegrationTest extends IntegrationTestCase {
+    public function testCreatingUserWithCommand() {
+        $command = $this->application->find('supla:user:create');
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs(['newuser@supla.org', 'ala123']);
+        $exitCode = $commandTester->execute([]);
+        $this->assertEquals(0, $exitCode);
+        $user = $this->getEntityManager()->getRepository(User::class)->findOneByEmail('newuser@supla.org');
+        $this->assertEquals('newuser@supla.org', $user->getEmail());
+        $this->assertTrue($user->isEnabled());
+    }
+
+    /** @depends testCreatingUserWithCommand */
+    public function testCreatingUserInAd() {
+        $this->assertCount(2, SuplaAutodiscoverMock::$requests); // 1st - checking if exists, 2nd - registering
+        $this->assertEquals(['email' => 'newuser@supla.org'], SuplaAutodiscoverMock::$requests[1]['post']);
+    }
+
+    public function testCreatingUserWithAdDisabled() {
+        SuplaAutodiscoverMock::clear(false, false);
+        $command = $this->application->find('supla:user:create');
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs(['localuser@supla.org', 'ala123']);
+        $exitCode = $commandTester->execute([]);
+        $this->assertEquals(0, $exitCode);
+        $user = $this->getEntityManager()->getRepository(User::class)->findOneByEmail('localuser@supla.org');
+        $this->assertEquals('localuser@supla.org', $user->getEmail());
+        $this->assertTrue($user->isEnabled());
+    }
+}
