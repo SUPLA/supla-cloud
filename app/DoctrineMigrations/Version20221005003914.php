2394817fa348ae6610691f999d100f088f96cbe5
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-10-05 01:52:20 +0200

    Update Version20221005003914.php

--- app/DoctrineMigrations/Version20221005003914.php
+++ app/DoctrineMigrations/Version20221005003914.php
@@ -13,7 +13,7 @@ class Version20210228201414 extends NoWayBackMigration
 {
     public function migrate() {
         $this->addSql("CREATE PROCEDURE `supla_set_scene_caption`(IN `_user_id` INT, IN `_scene_id` INT, IN `_caption` VARCHAR(255) CHARSET utf8mb4) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER UPDATE supla_scene SET caption = _caption WHERE id = _scene_id AND user_id = _user_id");
-        $this->addSql('ALTER TABLE `supla_scene` CHANGE `caption` `caption` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `supla_scene` CHANGE `caption` `caption` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
     }
 
 }
