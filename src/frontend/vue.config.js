6a769ec52eec19221a933b0af6d8345d95093843
Wojciech Frącz <fraczwojciech@gmail.com>
2022-10-21 23:38:58 +0200

    Update vue to 2.7.13

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -13,6 +13,11 @@ module.exports = {
         proxy: 'http://supla.local'
     },
     configureWebpack: {
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
+            }
+        },
         plugins: [
             new webpack.ProvidePlugin({
                 jQuery: 'jquery',
