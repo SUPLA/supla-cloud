528a18cbda73b6f0d53e5e19f335d81c96c74ec8
Wojciech Frącz <fraczwojciech@gmail.com>
2023-02-20 12:35:52 +0100

    Upgrade cypress to 12.6

---
+++ src/frontend/cypress.config.js
@@ -0,0 +1,14 @@
+const {defineConfig} = require('cypress')
+
+module.exports = defineConfig({
+    fixturesFolder: 'tests/e2e/fixtures',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    videosFolder: 'tests/e2e/videos',
+    e2e: {
+        baseUrl: 'http://localhost:8008',
+        setupNodeEvents(on, config) {
+        },
+        specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'tests/e2e/support/index.js',
+    },
+})
