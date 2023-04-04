eddaafb28018ce455c5f7e2989d1d2f6a73de702
Wojciech Frącz <fraczwojciech@gmail.com>
2023-04-04 13:30:16 +0200

    Improve account request deletion form for apps

--- src/frontend/src/login/request-account-deletion.vue
+++ src/frontend/src/login/request-account-deletion.vue
@@ -2,53 +2,63 @@
     <div class="container text-center">
         <h1 v-title>{{ $t('We will miss you!') }}</h1>
         <p class="text-center">{{ $t('Deleting your account will result also in deletion of all your data, including your connected devices, configured channels, direct links and measurement history. Deleting an account is irreversible.') }}</p>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label for="username">{{ $t('Your email') }}</label>
-                    <input type="email" id="username" class="form-control" autocomplete="new-password" v-model="username">
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label for="password">{{ $t('Password') }}</label>
-                    <input type="password" id="password" class="form-control" autocomplete="new-password" v-model="password">
-                </div>
-            </div>
+        <div v-if="isSent">
+            <fa icon="check" fixed-width size="xl"/>
+            <br>
+            <p class="my-3">{{ $t('We have sent you an e-mail message with a delete confirmation link. Just to be sure!') }}</p>
+            <router-link to="?ack=true" class="btn btn-green btn-lg">
+                {{ $t('Close') }}
+            </router-link>
         </div>
-        <div class="row">
-            <div class="col-sm-6 text-left">
-                <router-link to="/" class="btn btn-green btn-lg">
-                    {{ $t('No, take me back!') }}
-                </router-link>
+        <div v-else>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="username">{{ $t('Your email') }}</label>
+                        <input type="email" id="username" class="form-control" autocomplete="new-password" v-model="username">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="password">{{ $t('Password') }}</label>
+                        <input type="password" id="password" class="form-control" autocomplete="new-password" v-model="password">
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-6 text-right">
-                <div v-if="captchaEnabled">
-                    <invisible-recaptcha
-                        :sitekey="captchaSiteKey"
-                        :callback="checkCaptcha"
-                        id="registerRecaptcha"
-                        btn-class="btn btn-danger"
-                        :disabled="isBusy"
-                        :form-valid="!!requestData">
-                        <template>
+            <div class="row">
+                <div class="col-sm-6 text-left mb-3">
+                    <router-link to="/" class="btn btn-green btn-lg btn-take-me-back">
+                        {{ $t('No, take me back!') }}
+                    </router-link>
+                </div>
+                <div class="col-sm-6 text-right mb-3">
+                    <div v-if="captchaEnabled">
+                        <invisible-recaptcha
+                            :sitekey="captchaSiteKey"
+                            :callback="checkCaptcha"
+                            id="registerRecaptcha"
+                            btn-class="btn btn-danger"
+                            :disabled="isBusy"
+                            :form-valid="!!requestData">
+                            <template>
+                                <span v-if="!isBusy">
+                                    {{ $t('Delete my account') }}
+                                </span>
+                                <button-loading-dots v-else></button-loading-dots>
+                            </template>
+                        </invisible-recaptcha>
+                    </div>
+                    <div v-else>
+                        <button type="button"
+                            @click="confirmDeletion()"
+                            :disabled="!requestData"
+                            class="btn btn-danger">
                             <span v-if="!isBusy">
                                 {{ $t('Delete my account') }}
                             </span>
                             <button-loading-dots v-else></button-loading-dots>
-                        </template>
-                    </invisible-recaptcha>
-                </div>
-                <div v-else>
-                    <button type="button"
-                        @click="confirmDeletion()"
-                        :disabled="!requestData"
-                        class="btn btn-danger">
-                        <span v-if="!isBusy">
-                            {{ $t('Delete my account') }}
-                        </span>
-                        <button-loading-dots v-else></button-loading-dots>
-                    </button>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -56,7 +66,7 @@
 </template>
 
 <script>
-    import {errorNotification, successNotification} from "../common/notifier";
+    import {errorNotification} from "../common/notifier";
     import Vue from "vue";
     import InvisibleRecaptcha from "@/register/invisible-recaptcha";
 
@@ -67,6 +77,7 @@
                 username: '',
                 password: '',
                 isBusy: false,
+                isSent: false,
                 captchaEnabled: Vue.config.external.recaptchaEnabled,
                 captchaSiteKey: Vue.config.external.recaptchaSiteKey,
             };
@@ -79,13 +90,7 @@
                 this.isBusy = true;
                 const requestData = {...this.requestData, captchaCode};
                 this.$http.put('account-deletion', requestData, {skipErrorHandler: [400]})
-                    .then(() => {
-                        successNotification(
-                            this.$t('Successful'),
-                            this.$t('We have sent you an e-mail message with a delete confirmation link. Just to be sure!')
-                        );
-                        this.$router.push({name: 'login'});
-                    })
+                    .then(() => this.isSent = true)
                     .catch(() => errorNotification(this.$t('Error'), this.$t('Invalid username or password')))
                     .finally(() => this.isBusy = false);
             },
@@ -102,9 +107,25 @@
     };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    @import "../styles/mixins";
+
     p {
         font-size: 1.3em;
         margin-bottom: 2em;
+    }
+
+    @include on-xs-and-down {
+        .col-sm-6 {
+            text-align: center !important;
+        }
+    }
+</style>
+
+<style lang="scss">
+    .smartphone-webview {
+        .btn-take-me-back {
+            display: none;
+        }
     }
 </style>
--- src/frontend/src/styles/fontawesome.js
+++ src/frontend/src/styles/fontawesome.js
@@ -2,8 +2,19 @@ import Vue from "vue";
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faSquare} from '@fortawesome/free-regular-svg-icons'
-import {faCheckSquare} from '@fortawesome/free-solid-svg-icons'
+import {
+    faCheck,
+    faCheckSquare,
+    faDownload,
+    faGear,
+    faKey,
+    faPuzzlePiece,
+    faShieldHalved,
+    faSignIn,
+    faSignOut,
+    faTimesCircle
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faSquare, faCheckSquare);
+library.add(faSquare, faCheckSquare, faGear, faDownload, faSignOut, faSignIn, faShieldHalved, faPuzzlePiece, faKey, faTimesCircle, faCheck);
 
 Vue.component('fa', FontAwesomeIcon)
