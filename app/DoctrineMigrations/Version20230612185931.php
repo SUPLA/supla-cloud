dabae00cf79914c8560f1f79bc3380800e891bef
Wojciech Frącz <fraczwojciech@gmail.com>
2023-06-12 21:00:55 +0200

    Add supla_settings_string

---
+++ app/DoctrineMigrations/Version20230612185931.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Supla\Migrations;
+
+/**
+ * supla_settings_string.
+ */
+class Version20230612185931 extends NoWayBackMigration {
+    public function migrate() {
+        $this->addSql('CREATE TABLE supla_settings_string (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, value VARCHAR(1024) NOT NULL, UNIQUE INDEX UNIQ_814604C95E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+}
---
+++ src/SuplaBundle/Repository/SettingsStringRepository.php
@@ -0,0 +1,30 @@
+<?php
+namespace SuplaBundle\Repository;
+
+use Assert\Assertion;
+use Doctrine\ORM\EntityRepository;
+use SuplaBundle\Entity\Main\SettingsString;
+
+class SettingsStringRepository extends EntityRepository {
+    public function getValue(string $name): string {
+        $setting = $this->findOneBy(['name' => $name]);
+        Assertion::notNull($setting, 'No setting value for ' . $name);
+        return $setting->getValue();
+    }
+
+    public function hasValue(string $name): bool {
+        $setting = $this->findOneBy(['name' => $name]);
+        return $setting !== null;
+    }
+
+    public function setValue(string $name, string $value): void {
+        if ($this->hasValue($name)) {
+            $setting = $this->findOneBy(['name' => $name]);
+        } else {
+            $setting = new SettingsString($name, $value);
+        }
+        $setting->setValue($value);
+        $this->_em->persist($setting);
+        $this->_em->flush();
+    }
+}
