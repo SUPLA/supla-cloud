fab6c0ad6661fb2db5c00b3f9cbaac25d2c05091
Wojciech Frącz <fraczwojciech@gmail.com>
2023-09-26 08:41:20 +0200

    Simplify migration for adding supla_iodevice.user_config field

--- app/DoctrineMigrations/Version20230604004315.php
+++ app/DoctrineMigrations/Version20230604004315.php
@@ -24,10 +24,9 @@ namespace Supla\Migrations;
  */
 class Version20230604004315 extends NoWayBackMigration {
     public function migrate() {
-        $this->addSql("ALTER TABLE supla_iodevice ADD user_config VARCHAR(2048) DEFAULT NULL");
+        $this->addSql("ALTER TABLE supla_iodevice ADD user_config VARCHAR(4096) DEFAULT NULL");
         $this->addSql("CREATE PROCEDURE `supla_set_device_user_config`(IN `_user_id` INT, IN `_device_id` INT, IN `_user_config` VARCHAR(4096) CHARSET utf8mb4, IN `_md5` VARCHAR(32)) BEGIN UPDATE supla_iodevice SET user_config = _user_config WHERE id = _device_id AND user_id = _user_id AND MD5(IFNULL(user_config, '')) = _md5; SELECT STRCMP(user_config, _user_config) FROM supla_iodevice WHERE id = _device_id AND user_id = _user_id; END");
         $this->addSql("CREATE PROCEDURE `supla_set_channel_user_config`(IN `_user_id` INT, IN `_channel_id` INT, IN `_user_config` VARCHAR(4096) CHARSET utf8mb4, IN `_md5` VARCHAR(32)) BEGIN UPDATE supla_dev_channel SET user_config = _user_config WHERE id = _channel_id AND user_id = _user_id AND MD5(IFNULL(user_config, '')) = _md5; SELECT STRCMP(user_config, _user_config) FROM supla_dev_channel WHERE id = _channel_id AND user_id = _user_id; END");
-        $this->addSql("ALTER TABLE supla_iodevice CHANGE `user_config` `user_config` VARCHAR(4096) DEFAULT NULL");
         $this->addSql("ALTER TABLE supla_dev_channel CHANGE `user_config` `user_config` VARCHAR(4096) DEFAULT NULL");
     }
 }
