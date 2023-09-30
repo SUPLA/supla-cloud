dd0795d8475eb9423631df082d355018843ff632
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-09-30 23:12:24 +0200

    Modification of procedures responsible for changing the configuration of channels/devices.

--- app/DoctrineMigrations/Version20230604004315.php
+++ app/DoctrineMigrations/Version20230604004315.php
@@ -19,14 +19,14 @@ namespace Supla\Migrations;
 
 /**
  * New field supla_iodevice.user_config
- * New procedure supla_set_device_user_config
- * New procedure supla_set_channel_user_config
+ * New procedure supla_set_device_json_config
+ * New procedure supla_set_channel_json_config
  */
 class Version20230604004315 extends NoWayBackMigration {
     public function migrate() {
-        $this->addSql("ALTER TABLE supla_iodevice ADD user_config VARCHAR(4096) DEFAULT NULL");
-        $this->addSql("CREATE PROCEDURE `supla_set_device_user_config`(IN `_user_id` INT, IN `_device_id` INT, IN `_user_config` VARCHAR(4096) CHARSET utf8mb4, IN `_md5` VARCHAR(32)) BEGIN UPDATE supla_iodevice SET user_config = _user_config WHERE id = _device_id AND user_id = _user_id AND MD5(IFNULL(user_config, '')) = _md5; SELECT STRCMP(user_config, _user_config) FROM supla_iodevice WHERE id = _device_id AND user_id = _user_id; END");
-        $this->addSql("CREATE PROCEDURE `supla_set_channel_user_config`(IN `_user_id` INT, IN `_channel_id` INT, IN `_user_config` VARCHAR(4096) CHARSET utf8mb4, IN `_md5` VARCHAR(32)) BEGIN UPDATE supla_dev_channel SET user_config = _user_config WHERE id = _channel_id AND user_id = _user_id AND MD5(IFNULL(user_config, '')) = _md5; SELECT STRCMP(user_config, _user_config) FROM supla_dev_channel WHERE id = _channel_id AND user_id = _user_id; END");
+        $this->addSql("ALTER TABLE supla_iodevice ADD user_config VARCHAR(4096) DEFAULT NULL, ADD properties VARCHAR(2048) DEFAULT NULL");
+        $this->addSql("CREATE PROCEDURE `supla_set_device_json_config`(IN `_user_id` INT, IN `_device_id` INT, IN `_user_config` VARCHAR(4096) CHARSET utf8mb4, IN `_user_config_md5` VARCHAR(32), IN `_properties` VARCHAR(2048) CHARSET utf8mb4, IN `_properties_md5` VARCHAR(32)) BEGIN UPDATE supla_iodevice SET user_config = _user_config, properties = _properties WHERE id = _device_id AND user_id = _user_id AND MD5(IFNULL(user_config, '')) = _user_config_md5 AND MD5(IFNULL(properties, '')) = _properties_md5; SELECT ABS(STRCMP(user_config, _user_config))+ABS(STRCMP(properties, _properties)) FROM supla_iodevice WHERE id = _device_id AND user_id = _user_id; END");
+        $this->addSql("CREATE PROCEDURE `supla_set_channel_json_config`(IN `_user_id` INT, IN `_channel_id` INT, IN `_user_config` VARCHAR(4096) CHARSET utf8mb4, IN `_user_config_md5` VARCHAR(32), IN `_properties` VARCHAR(2048) CHARSET utf8mb4, IN `_properties_md5` VARCHAR(32)) BEGIN UPDATE supla_dev_channel SET user_config = _user_config, properties = _properties WHERE id = _channel_id AND user_id = _user_id AND MD5(IFNULL(user_config, '')) = _user_config_md5 AND MD5(IFNULL(properties, '')) = _properties_md5; SELECT ABS(STRCMP(user_config, _user_config))+ABS(STRCMP(properties, _properties)) FROM supla_dev_channel WHERE id = _channel_id AND user_id = _user_id; END");
         $this->addSql("ALTER TABLE supla_dev_channel CHANGE `user_config` `user_config` VARCHAR(4096) DEFAULT NULL");
     }
 }
