7dbcddb0d4e29798a4060f489ac443d2a4edeacd
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-12-28 10:14:05 +0100

    Update Version20231219083453.php

--- src/SuplaBundle/Migrations/Migration/2023/Version20231219083453.php
+++ src/SuplaBundle/Migrations/Migration/2023/Version20231219083453.php
@@ -22,6 +22,7 @@ use SuplaBundle\Migrations\NoWayBackMigration;
 
 /**
  * Migrate HVAC mode AUTO to HEAT_COOL.
+ * Add supla_oauth_add_token_for_alexa_discover procedure.
  */
 class Version20231219083453 extends NoWayBackMigration {
     public function migrate() {
@@ -57,5 +58,44 @@ class Version20231219083453 extends NoWayBackMigration {
                 }
             }
         }
+
+        $this->addSql(<<<PROCEDURE
+            CREATE PROCEDURE `supla_oauth_add_token_for_alexa_discover`(IN `_user_id` INT)
+BEGIN
+   DECLARE characters VARCHAR(62) DEFAULT 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+   DECLARE i INT DEFAULT 1;
+   DECLARE svr VARCHAR(168) DEFAULT '';
+   DECLARE token VARCHAR(255) DEFAULT NULL;
+   DECLARE client_id INT DEFAULT 0;
+
+   SELECT t.client_id, SUBSTRING_INDEX(t.token, '.', -1) INTO client_id, svr FROM `supla_oauth_refresh_tokens` t 
+          WHERE t.user_id = _user_id AND t.client_id IN (SELECT id FROM `supla_oauth_clients` WHERE name = 'AMAZON ALEXA') 
+          ORDER BY expires_at DESC LIMIT 1;
+
+   
+   SELECT t.token INTO token FROM `supla_oauth_access_tokens` t WHERE t.client_id IS NULL AND name = 'ALEXA DISCOVER' AND user_id = _user_id AND expires_at - UNIX_TIMESTAMP() >= 60;
+
+   IF token IS NOT NULL THEN
+     SELECT token;
+   ELSE
+     SET token = '';
+     
+     WHILE i <= 86 DO
+        SET token = CONCAT(token, SUBSTRING(characters, FLOOR(RAND() * 62) + 1, 1));
+        SET i = i + 1;
+     END WHILE;
+    
+     IF client_id > 0 THEN
+       SET token = CONCAT(token, '.', svr);
+    
+       INSERT INTO `supla_oauth_access_tokens`(`client_id`, `user_id`, `token`, `expires_at`, `scope`, `name`, `access_id`) VALUES 
+           (NULL, _user_id, token, UNIX_TIMESTAMP()+3600, 'channels_r iodevices_r locations_r account_r scenes_r', 'ALEXA DISCOVER', NULL);
+    
+       SELECT token;
+     END IF;
+   END IF;
+END
+PROCEDURE
+        );
     }
 }
