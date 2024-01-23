7ad001f8d4d560e31816ec0db3a57a5aefbc4562
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2024-01-23 15:07:47 +0100

    New procedure and field size change.

--- src/SuplaBundle/Migrations/Migration/2024/Version20240117202218.php
+++ src/SuplaBundle/Migrations/Migration/2024/Version20240117202218.php
@@ -22,10 +22,15 @@ use SuplaBundle\Migrations\NoWayBackMigration;
 /**
  * New table: supla_gpm_measurement_log.
  * New table: supla_gpm_meter_log.
+ * The supla_add_channel procedure has been extended with the 'alt_icon' parameter.
+ * The size of the "flags" variable has been increased, from INT to UNSIGNED BIGINT
  */
 class Version20240117202218 extends NoWayBackMigration {
     public function migrate() {
         $this->addSql('CREATE TABLE supla_gpm_measurement_log (channel_id INT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:stringdatetime)\', open_value DOUBLE PRECISION NOT NULL, close_value DOUBLE PRECISION NOT NULL, avg_value DOUBLE PRECISION NOT NULL, max_value DOUBLE PRECISION NOT NULL, min_value DOUBLE PRECISION NOT NULL, PRIMARY KEY(channel_id, date)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE supla_gpm_meter_log (channel_id INT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:stringdatetime)\', counter DOUBLE PRECISION NOT NULL, calculated_value DOUBLE PRECISION NOT NULL, PRIMARY KEY(channel_id, date)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB');
+        $this->addSql('DROP PROCEDURE `supla_add_channel`');
+        $this->addSql('CREATE PROCEDURE `supla_add_channel`(IN `_type` INT, IN `_func` INT, IN `_param1` INT, IN `_param2` INT, IN `_param3` INT, IN `_user_id` INT, IN `_channel_number` INT, IN `_iodevice_id` INT, IN `_flist` INT, IN `_flags` INT, IN `_alt_icon` INT) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER BEGIN INSERT INTO `supla_dev_channel` (`type`, `func`, `param1`, `param2`, `param3`, `user_id`, `channel_number`, `iodevice_id`, `flist`, `flags`, `alt_icon`) VALUES (_type, _func, _param1, _param2, _param3, _user_id, _channel_number, _iodevice_id, _flist, _flags, _alt_icon); END');
+        $this->addSql('ALTER TABLE `supla_dev_channel` CHANGE `flags` `flags` BIGINT UNSIGNED NULL DEFAULT NULL');
     }
 }
