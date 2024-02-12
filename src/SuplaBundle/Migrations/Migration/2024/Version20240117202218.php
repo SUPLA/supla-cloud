5970341548ef109295f1ed5f489624003891d7de
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2024-02-12 10:59:51 +0100

    Profile name is nullable

--- src/SuplaBundle/Migrations/Migration/2024/Version20240117202218.php
+++ src/SuplaBundle/Migrations/Migration/2024/Version20240117202218.php
@@ -121,7 +121,7 @@ PROCEDURE
             END
 PROCEDURE
         );
-        $this->sqlSql('ALTER TABLE `supla_client` ADD `profile_name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL AFTER `devel_env`');
+        $this->sqlSql('ALTER TABLE `supla_client` ADD `profile_name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci AFTER `devel_env`');
         $this->addSql('DROP PROCEDURE `supla_update_push_notification_client_token`');
         $this->addSql('CREATE PROCEDURE `supla_update_push_notification_client_token`(IN `_user_id` INT, IN `_client_id` INT, IN `_token` VARCHAR(255) CHARSET utf8mb4, IN `_platform` TINYINT, IN `_app_id` INT, IN `_devel_env` TINYINT, IN `_profile_name` VARCHAR(50) CHARSET utf8mb4) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER UPDATE supla_client SET push_token = _token, push_token_update_time = UTC_TIMESTAMP(), platform = _platform, app_id = _app_id, devel_env = _devel_env, profile_name = _profile_name WHERE id = _client_id AND user_id = _user_id');
     }
