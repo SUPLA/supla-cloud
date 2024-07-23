f1e9a37062e67f06868854de1b7d12e5d36e1f32
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2024-07-23 16:40:32 +0200

    Add supla_update_subdevice procedure

--- src/SuplaBundle/Migrations/Migration/2024/Version20240701141901.php
+++ src/SuplaBundle/Migrations/Migration/2024/Version20240701141901.php
@@ -23,6 +23,7 @@ use SuplaBundle\Migrations\NoWayBackMigration;
  * Add a pairing_result field
  * Add supla_update_device_pairing_result procedure
  * Create table supla_subdevice
+ * Add supla_update_subdevice procedure
  */
 class Version20240701141901 extends NoWayBackMigration {
     public function migrate() {
@@ -31,5 +32,7 @@ class Version20240701141901 extends NoWayBackMigration {
         $this->addSql('CREATE PROCEDURE `supla_update_device_pairing_result`(IN `_iodevice_id` INT, IN `_pairing_result` VARCHAR(512) CHARSET utf8mb4) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER UPDATE `supla_iodevice` SET `pairing_result` = _pairing_result WHERE `id` = _iodevice_id');
         $this->addSql('CREATE TABLE supla_subdevice (id INT NOT NULL, iodevice_id INT NOT NULL, name VARCHAR(200) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, software_version VARCHAR(20) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, product_code VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, serial_number VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_698D8D2F125F95D6 (iodevice_id), PRIMARY KEY(id, iodevice_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE supla_subdevice ADD CONSTRAINT FK_698D8D2F125F95D6 FOREIGN KEY (iodevice_id) REFERENCES supla_iodevice (id) ON DELETE CASCADE');
+        $this->addSql('DROP PROCEDURE IF EXISTS `supla_update_subdevice`');
+        $this->addSql('CREATE PROCEDURE `supla_update_subdevice`(IN `_id` INT, IN `_iodevice_id` INT, IN `_name` VARCHAR(200) CHARSET utf8mb4, IN `_software_version` VARCHAR(20) CHARSET utf8mb4, IN `_product_code` VARCHAR(50) CHARSET utf8mb4, IN `_serial_number` VARCHAR(50) CHARSET utf8mb4) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER INSERT INTO supla_subdevice (id, iodevice_id, name, software_version, product_code, serial_number) VALUES (_id, _iodevice_id, NULLIF(_name, \'\'), NULLIF(_software_version, \'\'), NULLIF(_product_code, \'\'), NULLIF(_serial_number, \'\')) ON DUPLICATE KEY UPDATE name = NULLIF(_name, \'\'), software_version = NULLIF(_software_version, \'\'), product_code = NULLIF(_product_code, \'\'), serial_number = NULLIF(_serial_number, \'\')');
     }
 }
