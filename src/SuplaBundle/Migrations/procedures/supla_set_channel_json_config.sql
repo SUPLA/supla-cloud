282ec72bd179af09e65d84dde8397c76a1f990e9
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2024-09-24 17:22:12 +0200

    Illegal mix of collations

--- src/SuplaBundle/Migrations/procedures/supla_set_channel_json_config.sql
+++ src/SuplaBundle/Migrations/procedures/supla_set_channel_json_config.sql
@@ -1,7 +1,7 @@
 DROP PROCEDURE IF EXISTS `supla_set_channel_json_config`;
 
-CREATE PROCEDURE `supla_set_channel_json_config`(IN `_user_id` INT, IN `_channel_id` INT, IN `_user_config` TEXT,
-                                                 IN `_user_config_md5` VARCHAR(32), IN `_properties` TEXT,
+CREATE PROCEDURE `supla_set_channel_json_config`(IN `_user_id` INT, IN `_channel_id` INT, IN `_user_config` TEXT CHARSET utf8mb4,
+                                                 IN `_user_config_md5` VARCHAR(32), IN `_properties` TEXT CHARSET utf8mb4,
                                                  IN `_properties_md5` VARCHAR(32))
     NOT DETERMINISTIC
     CONTAINS SQL SQL SECURITY DEFINER
@@ -13,7 +13,7 @@ BEGIN
       AND user_id = _user_id
       AND MD5(IFNULL(user_config, '')) = _user_config_md5
       AND MD5(IFNULL(properties, '')) = _properties_md5;
-    SELECT ABS(STRCMP(user_config, _user_config)) + ABS(STRCMP(properties, _properties))
+    SELECT ABS(STRCMP(user_config, _user_config COLLATE utf8mb4_unicode_ci)) + ABS(STRCMP(properties, _properties COLLATE utf8mb4_unicode_ci))
     FROM supla_dev_channel
     WHERE id = _channel_id
       AND user_id = _user_id;
