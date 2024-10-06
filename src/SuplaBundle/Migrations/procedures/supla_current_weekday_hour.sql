9b1bfe96e745e1ac6065bcaa33a388347d1de0e4
Wojciech Frącz <fraczwojciech@gmail.com>
2024-10-06 13:02:09 +0200

    Replace CONTAINS SQL with NO SQL

--- src/SuplaBundle/Migrations/procedures/supla_current_weekday_hour.sql
+++ src/SuplaBundle/Migrations/procedures/supla_current_weekday_hour.sql
@@ -2,7 +2,7 @@ DROP FUNCTION IF EXISTS supla_current_weekday_hour;
 
 CREATE FUNCTION supla_current_weekday_hour(`user_timezone` VARCHAR(50))
     RETURNS VARCHAR(3)
-    CONTAINS SQL
+    NO SQL
 BEGIN
     DECLARE current_weekday INT;
     DECLARE current_hour INT;
