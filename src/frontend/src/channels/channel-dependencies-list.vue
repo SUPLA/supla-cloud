0f6a79d0d10e652612bbd7cf787853a39962dd2d
Wojciech Frącz <fraczwojciech@gmail.com>
2025-04-17 15:24:42 +0200

    Move dependencies to the store

--- src/frontend/src/channels/channel-dependencies-list.vue
+++ src/frontend/src/channels/channel-dependencies-list.vue
@@ -3,10 +3,10 @@
         <h3 class="text-center">{{ $t('Dependencies') }}</h3>
         <div class="pt-3">
             <ul class="list-group m-0">
-                <li v-for="dep in dependencies" :key="dep.id" class="list-group-item">
-                    <div class="small text-muted">{{ $t(dep.label) }}</div>
-                    <router-link :to="{name: 'channel', params: {id: dep.channel.id}}">
-                        {{ channelTitle(dep.channel) }}
+                <li v-for="dep in dependencies" :key="dep.role" class="list-group-item">
+                    <div class="small text-muted">{{ $t(dependencyLabel(dep)) }}</div>
+                    <router-link :to="{name: 'channel', params: {id: dep.channelId}}">
+                        {{ channelTitle(channels[dep.channelId]) }}
                     </router-link>
                 </li>
             </ul>
@@ -18,10 +18,17 @@
     import {useChannelsStore} from "@/stores/channels-store";
     import {computed} from "vue";
     import {channelTitle} from "@/common/filters";
+    import {useChannelsDependenciesStore} from "@/stores/channels-dependencies-store";
+    import {storeToRefs} from "pinia";
+    import ChannelType from "@/common/enums/channel-type";
 
     const props = defineProps({
         channel: Object,
     });
+
+    const depsStore = useChannelsDependenciesStore();
+    const {all: channels} = storeToRefs(useChannelsStore());
+    const dependencies = computed(() => depsStore.forChannel(props.channel.id));
 
     const dependencyLabels = {
         masterThermostatChannelId_src: 'Master thermostat', // i18n
@@ -38,7 +45,8 @@
         openingSensorChannelId_dest: 'Opening sensor for channel', // i18n
         openingSensorSecondaryChannelId_src: 'Partial opening sensor', // i18n
         openingSensorSecondaryChannelId_dest: 'Partially opened sensor for channel', // i18n
-        heatOrColdSourceSwitchChannelId: 'Heat or cold source switch', // i18n
+        heatOrColdSourceSwitchChannelId_src: 'Heat or cold source switch', // i18n
+        heatOrColdSourceSwitchChannelId_dest: 'Heat or cold source switch for channel', // i18n
         relatedRelayChannelId_src: 'Associated measured channel', // i18n
         relatedRelayChannelId_dest: 'Associated measurement channel', // i18n
         relatedChannelId_src: 'Action trigger', // i18n
@@ -49,55 +57,17 @@
         levelSensorChannelIds_dest: 'Level sensor for container', // i18n
     };
 
-    const hiddenRelations = ['relatedMeterChannelId'];
-
-    const dependencyLabel = (role, direction) => dependencyLabels[`${role}_${direction}`] || dependencyLabels[role] || '';
-
-    const channelsStore = useChannelsStore();
-
-    const dependencies = computed(() => {
-        const deps = {};
-
-        // own dependencies
-        Object.keys(props.channel.config)
-            .filter((key) => key.endsWith('ChannelId'))
-            .filter((key) => !hiddenRelations.includes(key))
-            .filter((key) => props.channel.config[key] > 0)
-            .map((role) => ({
-                id: `ch_${role}_${props.channel.config[role]}`,
-                role,
-                label: dependencyLabel(role, 'src'),
-                channel: channelsStore.all[props.channel.config[role]],
-            }))
-            .forEach((dep) => deps[dep.id] = dep);
-
-        Object.keys(props.channel.config)
-            .filter((key) => key.endsWith('ChannelIds'))
-            .filter((key) => !hiddenRelations.includes(key))
-            .filter((key) => props.channel.config[key]?.length > 0)
-            .map((role) => props.channel.config[role].map((channelId) => ({
-                id: `ch_${role}_${channelId}`,
-                role,
-                label: dependencyLabel(role, 'src'),
-                channel: channelsStore.all[channelId],
-            })))
-            .forEach((depList) => depList.forEach((dep) => deps[dep.id] = dep));
-
-        // channels that refer to this one
-        Object.values(channelsStore.all).forEach((channel) => {
-            Object.keys(channel.config)
-                .filter((key) => key.endsWith('ChannelId') || key.endsWith('ChannelIds'))
-                .filter((key) => !hiddenRelations.includes(key))
-                .filter((key) => Array.isArray(channel.config[key]) ? channel.config[key].includes(props.channel.id) : channel.config[key] === props.channel.id)
-                .map((role) => ({
-                    id: `ch_${role}_${channel.id}`,
-                    role,
-                    label: dependencyLabel(role, 'dest'),
-                    channel,
-                }))
-                .forEach((dep) => deps[dep.id] = dep);
-        });
-
-        return Object.values(deps);
-    });
+    const dependencyLabel = (dep) => {
+        let dir = 'src';
+        if ([ChannelType.SENSORNO, ChannelType.THERMOMETER, ChannelType.HUMIDITYANDTEMPSENSOR, ChannelType.ACTION_TRIGGER].includes(props.channel.typeId)) {
+            dir = 'dest';
+        }
+        if (['masterThermostatChannelId', 'pumpSwitchChannelId', 'heatOrColdSourceSwitchChannelId'].includes(dep.role) && props.channel.config[dep.role] !== dep.channelId) {
+            dir = 'dest';
+        }
+        if ('relatedRelayChannelId' === dep.role && [ChannelType.RELAY, ChannelType.RELAY2XG5LA1A].includes(props.channel.typeId)) {
+            dir = 'dest';
+        }
+        return dependencyLabels[`${dep.role}_${dir}`] || dependencyLabels[dep.role] || '';
+    };
 </script>
---
+++ src/frontend/src/stores/channels-dependencies-store.js
@@ -0,0 +1,50 @@
+import {defineStore} from "pinia";
+import {useChannelsStore} from "@/stores/channels-store";
+import {computed} from "vue";
+
+const hiddenRelations = ['relatedMeterChannelId'];
+
+export const useChannelsDependenciesStore = defineStore('channelsDependencies', () => {
+    const channelsStore = useChannelsStore();
+
+    const dependencies = computed(() => {
+        return Object.values(channelsStore.list.reduce((acc, channel) => {
+            Object.keys(channel.config)
+                .filter((key) => key.endsWith('ChannelId'))
+                .filter((key) => !hiddenRelations.includes(key))
+                .filter((key) => channel.config[key] > 0)
+                .map((role) => ({
+                    id: `ch_${role}_${Math.min(channel.config[role], channel.id)}_${Math.max(channel.config[role], channel.id)}`,
+                    role,
+                    channel1Id: channel.id,
+                    channel2Id: channel.config[role],
+                }))
+                .forEach((dep) => acc[dep.id] = dep);
+            Object.keys(channel.config)
+                .filter((key) => key.endsWith('ChannelIds'))
+                .filter((key) => !hiddenRelations.includes(key))
+                .filter((key) => channel.config[key]?.length > 0)
+                .map((role) => channel.config[role].map((channelId) => ({
+                    id: `ch_${role}_${Math.min(channelId, channel.id)}_${Math.max(channelId, channel.id)}`,
+                    role,
+                    channel1Id: channel.id,
+                    channel2Id: channelId,
+                })))
+                .forEach((depList) => depList.forEach((dep) => acc[dep.id] = dep));
+            return acc;
+        }, {}));
+    });
+
+    const forChannel = computed(() => ((channelId) => dependencies.value
+            .filter((dep) => dep.channel1Id === channelId || dep.channel2Id === channelId)
+            .map((dep) => ({
+                role: dep.role,
+                channelId: dep.channel1Id === channelId ? dep.channel2Id : dep.channel1Id,
+            }))
+    ))
+
+    const $reset = () => {
+    };
+
+    return {dependencies, forChannel, $reset};
+});
