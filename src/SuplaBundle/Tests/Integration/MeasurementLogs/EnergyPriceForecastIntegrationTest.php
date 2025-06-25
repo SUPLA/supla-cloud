1b83dee33555143a02d628a54c9ad28fca44ae47
Wojciech Frącz <fraczwojciech@gmail.com>
2025-06-25 13:41:11 +0200

    Adjust tests to new PD GSZ parameter

--- src/SuplaBundle/Tests/Integration/MeasurementLogs/EnergyPriceForecastIntegrationTest.php
+++ src/SuplaBundle/Tests/Integration/MeasurementLogs/EnergyPriceForecastIntegrationTest.php
@@ -99,7 +99,7 @@ class EnergyPriceForecastIntegrationTest extends IntegrationTestCase {
         $client->apiRequestV3('GET', '/api/integrations/energy-price-forecast/parameters');
         $this->assertStatusCode(200, $client->getResponse());
         $parameters = json_decode($client->getResponse()->getContent(), true);
-        $this->assertEquals(['rce', 'fixing1', 'fixing2'], $parameters);
+        $this->assertEquals(['rce', 'pdgsz', 'fixing1', 'fixing2'], $parameters);
         $client->apiRequestV3('POST', '/api/channels', [
             'virtualChannelType' => VirtualChannelType::ENERGY_PRICE_FORECAST,
             'virtualChannelConfig' => [
@@ -157,7 +157,7 @@ class EnergyPriceForecastIntegrationTest extends IntegrationTestCase {
         $client->apiRequestV3('GET', '/api/integrations/energy-price-forecast/parameters');
         $this->assertStatusCode(200, $client->getResponse());
         $parameters = json_decode($client->getResponse()->getContent(), true);
-        $this->assertEquals(['rce'], $parameters);
+        $this->assertEquals(['rce', 'pdgsz'], $parameters);
         $client->apiRequestV3('POST', '/api/channels', [
             'virtualChannelType' => VirtualChannelType::ENERGY_PRICE_FORECAST,
             'virtualChannelConfig' => [
