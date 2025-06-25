f828ed783c1386758187d9d5294341631d7d1129
Wojciech Frącz <fraczwojciech@gmail.com>
2025-06-25 21:04:53 +0200

    Reorganize E2E tests seeder

--- src/frontend/tests/e2e/plugins/seeder.js
+++ src/frontend/tests/e2e/plugins/seeder.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const {ConnectionString} = require('connection-string');
 const path = require("path");
-const Importer = require('mysql-import');
+const fs = require('fs');
 
 module.exports = (on, config) => {
     on('task', {
@@ -19,22 +19,26 @@ module.exports = (on, config) => {
             connection.connect();
             return new Promise((resolve, reject) => {
                 connection.query(`DROP SCHEMA IF EXISTS ${database}; CREATE SCHEMA ${database}; USE ${database};`, async (error) => {
-                    connection.end();
                     if (error) {
                         reject(error);
                     } else {
-                        const importer = new Importer({
-                            host: connectionConfig.host,
-                            user: connectionConfig.user,
-                            password: connectionConfig.password,
-                            database
-                        });
                         const additionalFixtures = fixtures ? (Array.isArray(fixtures) ? fixtures : [fixtures]) : [];
                         const fixtureFiles = ['schema.sql', '00_required.sql', ...additionalFixtures];
                         for (const fileToImport of fixtureFiles) {
                             const fullPath = path.resolve(`./tests/e2e/plugins/data/${fileToImport}`);
-                            await importer.import(fullPath);
+                            const sqlContent = fs.readFileSync(fullPath, 'utf8');
+                            const sqlFixed = sqlContent.toString().replace(/DELIMITER ;?;/gm, '').replace(/;;/gm, ';')
+                            await new Promise((resolveQuery, rejectQuery) => {
+                                connection.query(sqlFixed, (error) => {
+                                    if (error) {
+                                        rejectQuery(error);
+                                    } else {
+                                        resolveQuery();
+                                    }
+                                });
+                            });
                         }
+                        connection.end();
                         resolve(true);
                     }
                 });
