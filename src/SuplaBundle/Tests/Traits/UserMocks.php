1e8b51b1be761b243d2d7b33b36ccf86e5bae409
Wojciech Frącz <fraczwojciech@gmail.com>
2026-01-07 16:36:11 +0100

    Deprecations free unit tests

--- src/SuplaBundle/Tests/Traits/UserMocks.php
+++ src/SuplaBundle/Tests/Traits/UserMocks.php
@@ -3,9 +3,9 @@
 namespace SuplaBundle\Tests\Traits;
 
 use PHPUnit\Framework\MockObject\MockObject;
+use SuplaBundle\Auth\Token\WebappToken;
 use SuplaBundle\Entity\Main\User;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 /**
  * @method MockObject createMock(string $className)
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 trait UserMocks {
     protected function getMockedTokenStorage(?User $user = null): TokenStorageInterface {
         $user = $user ?: $this->createMock(User::class);
-        $token = $this->createMock(TokenInterface::class);
+        $token = $this->createMock(WebappToken::class);
         $token->method('getUser')->willReturn($user);
         $userTokenStorage = $this->createMock(TokenStorageInterface::class);
         $userTokenStorage->method('getToken')->willReturn($token);
