11f4e2acbb1ad30f45a6869929cf6e6b0c29590b
Wojciech Frącz <fraczwojciech@gmail.com>
2026-01-09 13:52:28 +0100

    Get rid of SuplaBundle :rocket:

--- src/DependencyInjection/GitVersionDumper.php
+++ src/DependencyInjection/GitVersionDumper.php
@@ -15,7 +15,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-namespace SuplaBundle\DependencyInjection;
+namespace App\DependencyInjection;
 
 use Symfony\Component\Yaml\Yaml;
 
