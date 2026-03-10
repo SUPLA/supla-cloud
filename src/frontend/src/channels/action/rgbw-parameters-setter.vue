55f44167be456c238360e3c27aab1e0a35370740
Wojciech Frącz <fraczwojciech@gmail.com>
2026-03-10 23:04:33 +0100

    Allow to change RGB and color brightnes separately

--- src/frontend/src/channels/action/rgbw-parameters-setter.vue
+++ src/frontend/src/channels/action/rgbw-parameters-setter.vue
@@ -7,10 +7,10 @@
     </div>
     <AccordionRoot multiple v-model="openedSections" :disabled="disabled || rgbwCommand !== 'NOT_SET'">
       <AccordionItem v-if="hasColor && shouldDisplayColorBrightness" title-i18n="Color brightness" name="colorBrightness" :iconOpened="faCheckCircle">
-        <ColorBrightnessColorpicker v-if="colorBrightnessOpened" v-model="colorBrightness" :color="rgb" />
+        <ColorBrightnessColorpicker v-if="colorBrightnessOpened" v-model="colorBrightness" />
       </AccordionItem>
       <AccordionItem v-if="hasColor && shouldDisplayColor" title-i18n="Color" name="color" :iconOpened="faCheckCircle">
-        <ColorColorpicker v-if="rgbOpened" v-model="rgb" :brightness="colorBrightness" @onNewBrightness="model.color_brightness = $event" />
+        <ColorColorpicker v-if="rgbOpened" v-model="rgb" @onNewBrightness="model.color_brightness = $event" />
       </AccordionItem>
       <AccordionItem v-if="hasBrightness && shouldDisplayBrightness" title-i18n="White brightness" name="brightness" :iconOpened="faCheckCircle">
         <ColorBrightnessColorpicker v-if="brightnessOpened" v-model="brightness" />
